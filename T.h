814cb30d04b33fe08e7a1123e6a2e526a47e6e4a
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-02-14 11:46:40 -0500

    Initial checkin

---
+++ T.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+struct T
+{
+    T() noexcept { std::cout << "T()" << std::endl; }
+    T(int i) noexcept { std::cout << "T(" << i << ")" << std::endl; }
+    T(const std::string& s) noexcept { std::cout << "T(\"" << s << "\")" << std::endl; }
+    T(const T&) noexcept { std::cout << "T(const T&)" << std::endl; }
+    T(T&&) noexcept { std::cout << "T(&&)" << std::endl; }
+    T& operator = (const T&) noexcept { std::cout << "operator = (const T&)" << std::endl; return *this; }
+    T& operator = (T&&) noexcept { std::cout << "operator = (T&&)" << std::endl; return *this; }
+    ~T() noexcept { std::cout << "~T()" << std::endl; }
+};
