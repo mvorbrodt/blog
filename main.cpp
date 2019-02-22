436603ad8dca66b51ddcf7c5322ad9d1ba9ca1c7
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-02-22 12:47:05 -0500

    Cleanup and simplification of synchronization algorithm

--- main.cpp
+++ main.cpp
@@ -12,22 +12,17 @@ int main(int argc, char** argv)
 {
 	atomic_uint flag{0};
 
-	auto proc = [&](int thread, unsigned int thread_mask) {
+	auto proc = [&](int t, unsigned int thread_mask) {
 		for(int i = 0; i < COUNT;)
 		{
-			flag.fetch_or(thread_mask);
-			if(flag.load() == thread_mask)
+			if(flag.fetch_or(thread_mask) == 0)
 			{
-				cout << "T" << thread << " in critical section, i = " << i << endl;
-				flag.fetch_xor(thread_mask);
+				cout << "T" << t << " in critical section" << endl;
 				++i;
-				this_thread::yield();
 			}
-			else
-			{
-				flag.fetch_xor(thread_mask);
-				this_thread::yield();
-			}
+
+			flag.fetch_xor(thread_mask);
+			this_thread::yield();
 		}
 	};
 
