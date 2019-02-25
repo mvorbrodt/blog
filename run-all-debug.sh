7a87e687cea3b40073a7dc0fbe15c4d3906bb68f
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-02-25 03:42:12 -0500

    Update "echo" compiler options

--- run-all-debug.sh
+++ run-all-debug.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-echo "***************"
-echo "* Apple CLANG *"
-echo "***************"
+echo "**************************************************"
+echo "* Apple CLANG -O0 -march=native -std=c++17 -lc++ *"
+echo "**************************************************"
 echo
 rm -f playground
 clang -O0 -march=native -std=c++17 -lc++ main.cpp -o playground
 ./playground
 echo
 
-echo "********"
-echo "* LLVM *"
-echo "********"
+echo "*******************************************"
+echo "* LLVM -O0 -march=native -std=c++17 -lc++ *"
+echo "*******************************************"
 echo
 rm -f playground
 /usr/local/opt/llvm/bin/clang -O0 -march=native -std=c++17 -L/usr/local/opt/llvm/lib -lc++ main.cpp -o playground
 ./playground
 echo
 
-echo "*******"
-echo "* GCC *"
-echo "*******"
+echo "*********************************************"
+echo "* GCC -O0 -march=native -std=c++17 -lstdc++ *"
+echo "*********************************************"
 echo
 rm -f playground
 gcc -O0 -march=native -std=c++17 -lstdc++ main.cpp -o playground
--- run-all.sh
+++ run-all.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-echo "***************"
-echo "* Apple CLANG *"
-echo "***************"
+echo "*****************************************************"
+echo "* Apple CLANG -Ofast -march=native -std=c++17 -lc++ *"
+echo "*****************************************************"
 echo
 rm -f playground
 clang -Ofast -march=native -std=c++17 -lc++ main.cpp -o playground
 ./playground
 echo
 
-echo "********"
-echo "* LLVM *"
-echo "********"
+echo "**********************************************"
+echo "* LLVM -Ofast -march=native -std=c++17 -lc++ *"
+echo "**********************************************"
 echo
 rm -f playground
 /usr/local/opt/llvm/bin/clang -Ofast -march=native -std=c++17 -L/usr/local/opt/llvm/lib -lc++ main.cpp -o playground
 ./playground
 echo
 
-echo "*******"
-echo "* GCC *"
-echo "*******"
+echo "************************************************"
+echo "* GCC -Ofast -march=native -std=c++17 -lstdc++ *"
+echo "************************************************"
 echo
 rm -f playground
 gcc -Ofast -march=native -std=c++17 -lstdc++ main.cpp -o playground
