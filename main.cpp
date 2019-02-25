375ba408a1416d48361f4645f4b5894a11de60f0
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-02-25 06:53:55 -0500

    Better timer class

--- main.cpp
+++ main.cpp
@@ -1,106 +1,36 @@
-/*#include <stdio.h>
- #include <stdlib.h>
- #include <time.h>
- 
- int main()
- {
- srand((unsigned int)time(NULL));
- for(int n = 0; n < 10; ++n)
- printf("%d ", rand() % 1000);
- printf("\n");
- }*/
 #include <iostream>
-#include <random>
-#include <algorithm>
-#include <vector>
-#include <chrono>
+#include <sstream>
+#include "timer.h"
 using namespace std;
 using namespace chrono;
 
-const int COUNT = 1'000'000;
-
 int main()
 {
-	/* random_device MAY BE CRYPTOGRAPHICALLY STRONG */
-	random_device rd;
-	/* SEED mt19937 and mt19937_64 ENGINE WITH STRONG RANDOM NUMBER */
-	mt19937 mt(rd());
-	mt19937_64 mt64(rd());
-	/* CREATE RANDOM DISTRIBUTION OBJECTS FOR INTS AND DOUBLES */
-	uniform_int_distribution int_dist(-10, 10);
-	uniform_real_distribution real_dist(1.0, 10.0);
-
-	/* PRINT MIN AND MAX OF EACH RANDOM ENGINE */
-	cout << "random_device min = " << rd.min() << ", max = " << rd.max() << endl;
-	cout << "mt19937 min = " << mt.min() << ", max = " << mt.max() << endl;
-	cout << "mt19937_64 min = " << mt64.min() << ", max = " << mt64.max() << endl;
-
-	/* GENERATE 10 INTEGERS IN RANGE -10 TO 10 USING mt19937 GENERATOR */
-	for(int n = 0; n < 10; ++n)
-		cout << int_dist(mt) << " ";
-	cout << endl;
-
-	/* GENERATE 10 DOUBLES IN RANGE 1.0 TO 10.0 USING mt19937 GENERATOR */
-	for(int n = 0; n < 5; ++n)
-		cout << real_dist(mt) << " ";
-	cout << endl;
-
-	/* GENERATE A VECTOR OF CONSECUTIVE INTEGERS */
-	vector<int> v;
-	for(int n = 0; n < 10; ++n)
-		v.push_back(n);
-
-	/* PRINT IT */
-	cout << "vector of ints: ";
-	for(auto it : v) cout << it << " ";
-	cout << endl;
-
-	/* RANDOMLY SHUFFLE THE VECTOR OF INTEGERS USING mt19937 GENERATOR */
-	shuffle(begin(v), end(v), mt);
-
-	/* PRINT IT */
-	cout << "shuffled to   : ";
-	for(auto it : v) cout << it << " ";
-	cout << endl;
-	
-	/* ********************* */
-	/* BENCHMARK STARTS HERE */
-	/* ********************* */
-	cout << "generating " << COUNT << " random numbers..." << endl;
-
-	/* TEST PERFORMANCE OF random_device */
 	auto start = high_resolution_clock::now();
-	int result{0};
-	for(int i = 0; i < COUNT; ++i)
-		result += rd();
-	auto end = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(end - start).count() / 1000.0f;
-	cout << "random_device duration in ms = " << duration << endl;
+	auto duration = [&]() {
+		auto now = high_resolution_clock::now();
+		auto secs = duration_cast<seconds>(now - start).count();
+		stringstream ss;
+		ss << secs;
+		cout << "elapsed " << ss.str() << "s\t: ";
+	};
 
-	/* TEST PERFORMANCE OF mt19937 */
-	start = high_resolution_clock::now();
-	result = 0;
-	for(int i = 0; i < COUNT; ++i)
-		result += mt();
-	end = high_resolution_clock::now();
-	duration = duration_cast<microseconds>(end - start).count() / 1000.0f;
-	cout << "mt19937 duration in ms = " << duration << endl;
-
-	/* TEST PERFORMANCE OF uniform_int_distribution */
-	start = high_resolution_clock::now();
-	result = 0;
-	for(int i = 0; i < COUNT; ++i)
-		result += int_dist(mt);
-	end = high_resolution_clock::now();
-	duration = duration_cast<microseconds>(end - start).count() / 1000.0f;
-	cout << "uniform_int_distribution duration in ms = " << duration << endl;
-
-	/* TEST PERFORMANCE OF uniform_real_distribution */
-	start = high_resolution_clock::now();
-	result = 0;
-	for(int i = 0; i < COUNT; ++i)
-		result += real_dist(mt);
-	end = high_resolution_clock::now();
-	duration = duration_cast<microseconds>(end - start).count() / 1000.0f;
-	cout << "uniform_real_distribution duration in ms = " << duration << endl;
+	cout << "start" << endl;
+	timer t(1s);
+	auto e1 = t.set_interval(1, [&]() { duration(); cout << "interval 1" << endl; });
+	auto e2 = t.set_interval(2, [&]() { duration(); cout << "interval 2" << endl; });
+	auto e3 = t.set_timeout(3, [&]() { duration(); cout << "timeout 1" << endl; });
+	auto e4 = t.set_timeout(4, [&]() { duration(); cout << "timeout 2" << endl; });
+	auto e5 = t.set_timeout(5, [&]() { duration(); cout << "timeout that never happens" << endl; });
+	e5->signal(); // cancel this timeout
+	this_thread::sleep_for(5s);
+	cout << "pause intervals for 5s" << endl;
+	e1->signal(); // cancel this interval
+	e2->signal(); // cancel this interval
+	this_thread::sleep_for(5s);
+	cout << "resume intervals" << endl;
+	e1->reset(); // resume this interval
+	e2->reset(); // resume this interval
+	this_thread::sleep_for(5s);
+	cout << "end" << endl;
 }
--- timer.h
+++ timer.h
@@ -1,35 +1,110 @@
 #pragma once
 
 #include <thread>
+#include <chrono>
 #include <memory>
+#include <functional>
+#include <list>
+#include <cassert>
 #include "event.h"
 
-namespace timer
+class timer
 {
-	template<typename D, typename F, typename... Args>
-	std::shared_ptr<manual_event> set_timeout(D d, F f, Args&&... args)
+public:
+	timer(const std::chrono::seconds& tick)
+	: m_tick(tick), m_thread([this]() {
+		std::chrono::microseconds drift{0};
+		while(!m_event.wait_for(m_tick - drift))
+		{
+			auto start = std::chrono::high_resolution_clock::now();
+			for(auto& interval : m_intervals)
+			{
+				++interval.elapsed;
+				if(interval.elapsed == interval.interval)
+				{
+					interval.proc();
+					if(interval.event != nullptr)
+						interval.event->signal();
+					interval.elapsed = 0;
+				}
+			}
+			auto end = std::chrono::high_resolution_clock::now();
+			drift = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+		}
+	})
+	{}
+
+	~timer()
 	{
+		m_event.signal();
+		m_thread.join();
+	}
+
+	template<typename F, typename... Args>
+	std::shared_ptr<manual_event> set_timeout(unsigned int timeout_in_ticks, F f, Args&&... args)
+	{
+		assert(timeout_in_ticks > 0);
 		auto event = std::make_shared<manual_event>();
-		std::thread([=]()
+		auto proc = [=]() {
+			if(event->wait_for(std::chrono::milliseconds(0))) return;
+			f(args...);
+		};
+		m_intervals.insert(m_intervals.end(), { proc, timeout_in_ticks, 0, event });
+		return event;
+	}
+
+	template<typename F, typename... Args>
+	std::shared_ptr<manual_event> set_interval(unsigned int interval_in_ticks, F f, Args&&... args)
+	{
+		assert(interval_in_ticks > 0);
+		auto event = std::make_shared<manual_event>();
+		auto proc = [=]() {
+			if(event->wait_for(std::chrono::milliseconds(0))) return;
+			f(args...);
+		};
+		m_intervals.insert(m_intervals.end(), { proc, interval_in_ticks, 0, nullptr });
+		return event;
+	}
+
+private:
+	std::chrono::seconds m_tick;
+	manual_event m_event;
+	std::thread m_thread;
+
+	struct interval_ctx
+	{
+		std::function<void(void)> proc;
+		unsigned int interval;
+		unsigned int elapsed;
+		std::shared_ptr<manual_event> event;
+	};
+
+	std::list<interval_ctx> m_intervals;
+};
+
+template<typename D, typename F, typename... Args>
+std::shared_ptr<manual_event> set_timeout_thread(D d, F f, Args&&... args)
+{
+	auto event = std::make_shared<manual_event>();
+	std::thread([=]()
+	{
+		if(event->wait_for(d)) return;
+		f(args...);
+	}).detach();
+	return event;
+}
+
+template<typename D, typename F, typename... Args>
+std::shared_ptr<manual_event> set_interval_thread(D d, F f, Args&&... args)
+{
+	auto event = std::make_shared<manual_event>();
+	std::thread([=]()
+	{
+		while(true)
 		{
 			if(event->wait_for(d)) return;
 			f(args...);
-		}).detach();
-		return event;
-	}
-
-	template<typename D, typename F, typename... Args>
-	std::shared_ptr<manual_event> set_interval(D d, F f, Args&&... args)
-	{
-		auto event = std::make_shared<manual_event>();
-		std::thread([=]()
-		{
-			while(true)
-			{
-				if(event->wait_for(d)) return;
-				f(args...);
-			}
-		}).detach();
-		return event;
-	}
+		}
+	}).detach();
+	return event;
 }
