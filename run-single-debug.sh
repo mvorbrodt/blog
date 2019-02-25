2911672bd1ca7ea398e6ba8e5dc2140e5a64b43e
Martin Vorbrodt <martin@Martins-MBP-15.local>
2019-02-25 03:45:29 -0500

    Add "echo" compiler options

--- run-single-debug.sh
+++ run-single-debug.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "**************************************************"
+echo "* Apple CLANG -O0 -march=native -std=c++17 -lc++ *"
+echo "**************************************************"
+echo
 rm -f playground
 clang -O0 -march=native -std=c++17 -lc++ main.cpp -o playground
 ./playground
+echo
