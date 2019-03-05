83cde43cbddc5c2f7ab555e696e39537c08073fd
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-05 14:02:38 -0500

    Cleanup the run.sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,135 @@
 #!/bin/bash
 
+function build_clang {
+	echo "***********************************************"
+	echo "* CLANG -Ofast -march=native -std=c++2a -lc++ *"
+	echo "***********************************************"
+	echo
+	rm -f playground
+	clang++ \
+		-Ofast \
+		-march=native \
+		-std=c++2a \
+		-lc++ \
+		-ltbb \
+		main.cpp \
+		-o playground
+	./playground
+	echo
+}
+
+function build_clang_debug {
+	echo "********************************************"
+	echo "* CLANG -O0 -march=x86-64 -std=c++2a -lc++ *"
+	echo "********************************************"
+	echo
+	rm -f playground
+	clang++ \
+		-O0 \
+		-march=x86-64 \
+		-std=c++2a \
+		-lc++ \
+		-ltbb \
+		main.cpp \
+		-o playground
+	./playground
+	echo
+}
+
+function build_llvm {
+	echo "**********************************************"
+	echo "* LLVM -Ofast -march=native -std=c++2a -lc++ *"
+	echo "**********************************************"
+	echo
+	rm -f playground
+	clang++-7 \
+		-Ofast \
+		-march=native\
+		-std=c++2a \
+		-L/usr/local/opt/llvm/lib \
+		-lc++ \
+		-ltbb \
+		main.cpp \
+		-o playground
+	./playground
+	echo
+}
+
+function build_llvm_debug {
+	echo "*******************************************"
+	echo "* LLVM -O0 -march=x86-64 -std=c++2a -lc++ *"
+	echo "*******************************************"
+	echo
+	rm -f playground
+	clang++-7 \
+		-O0 \
+		-march=x86-64 \
+		-std=c++2a \
+		-L/usr/local/opt/llvm/lib \
+		-lc++ \
+		-ltbb \
+		main.cpp \
+		-o playground
+	./playground
+	echo
+}
+
+function build_gcc {
+	echo "************************************************"
+	echo "* G++ -Ofast -march=native -std=c++2a -lstdc++ *"
+	echo "************************************************"
+	echo
+	rm -f playground
+	g++-8 \
+		-Ofast \
+		-march=native \
+		-std=c++2a \
+		-I/usr/local/include \
+		-L/usr/local/opt/gcc/lib/ \
+		-L/usr/local/lib \
+		-lstdc++ \
+		-ltbb \
+		main.cpp \
+		-o playground
+	./playground
+	echo
+}
+
+function build_gcc_debug {
+	echo "*********************************************"
+	echo "* G++ -O0 -march=x86-64 -std=c++2a -lstdc++ *"
+	echo "*********************************************"
+	echo
+	rm -f playground
+	g++-8 \
+		-O0 \
+		-march=x86-64 \
+		-std=c++2a \
+		-I/usr/local/include \
+		-L/usr/local/opt/gcc/lib/ \
+		-L/usr/local/lib \
+		-lstdc++ \
+		-ltbb \
+		main.cpp \
+		-o playground
+	./playground
+	echo
+}
+
+function help {
+	echo "$0 {c|cd|l|ld|g|gd|a|ad}"
+	echo
+	echo "    c - CLANG (default)"
+	echo "   cd - CLANG Debug"
+	echo "    l - LLVM"
+	echo "   ld - LLVM Debug"
+	echo "    g - GCC"
+	echo "   gd - GCC Debug"
+	echo "    a - ALL"
+	echo "   ad - ALL Debug"
+	echo
+}
+
 if [ $# -eq 0 ]
 then
 	param='c'
@@ -11,61 +141,34 @@ then
 	param='help'
 fi
 
-rm -f playground
-
 if [ $param = 'c' ]
 then
-	echo "***********************************************"
-	echo "* CLANG -Ofast -march=native -std=c++2a -lc++ *"
-	echo "***********************************************"
-	echo
-	clang++ -Ofast -march=native -std=c++2a -lc++ -ltbb main.cpp -o playground
+	build_clang
 elif [ $param = 'cd' ]
 then
-	echo "******************************"
-	echo "* CLANG -O0 -std=c++2a -lc++ *"
-	echo "******************************"
-	echo
-	clang++ -O0 -std=c++2a -lc++ -ltbb main.cpp -o playground
+	build_clang_debug
 elif [ $param = 'l' ]
 then
-	echo "**********************************************"
-	echo "* LLVM -Ofast -march=native -std=c++2a -lc++ *"
-	echo "**********************************************"
-	echo
-	/usr/local/opt/llvm/bin/clang -Ofast -march=native -std=c++2a -L/usr/local/opt/llvm/lib -lc++ -ltbb main.cpp -o playground
+	build_llvm
 elif [ $param = 'ld' ]
 then
-	echo "*****************************"
-	echo "* LLVM -O0 -std=c++2a -lc++ *"
-	echo "*****************************"
-	echo
-	/usr/local/opt/llvm/bin/clang -O0 -std=c++2a -L/usr/local/opt/llvm/lib -lc++ -ltbb main.cpp -o playground
+	build_llvm_debug
 elif [ $param = 'g' ]
 then
-	echo "************************************************"
-	echo "* G++ -Ofast -march=native -std=c++2a -lstdc++ *"
-	echo "************************************************"
-	echo
-	/usr/local/opt/gcc/bin/g++-8 -Ofast -march=native -std=c++2a -I/usr/local/include -L/usr/local/opt/gcc/lib/ -L/usr/local/lib -lstdc++ -ltbb main.cpp -o playground
+	build_gcc
 elif [ $param = 'gd' ]
 then
-	echo "*******************************"
-	echo "* G++ -O0 -std=c++2a -lstdc++ *"
-	echo "*******************************"
-	echo
-	/usr/local/opt/gcc/bin/g++-8 -O0 -std=c++2a -I/usr/local/include -L/usr/local/opt/gcc/lib/ -L/usr/local/lib -lstdc++ -ltbb main.cpp -o playground
+	build_gcc_debug
+elif [ $param = 'a' ]
+then
+	build_clang
+	build_llvm
+	build_gcc
+elif [ $param = 'ad' ]
+then
+	build_clang_debug
+	build_llvm_debug
+	build_gcc_debug
 else
-	echo "$0 {c|cd|l|ld|g|gd}"
-	echo
-	echo "   c  - CLANG (default)"
-	echo "   cd - CLANG Debug"
-	echo "   l  - LLVM"
-	echo "   ld - LLVM Debug"
-	echo "   g  - GCC"
-	echo "   gd - GCC Debug"
-	echo
-	exit
+	help
 fi
-
-./playground
