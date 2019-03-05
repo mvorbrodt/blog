89becec9102fc6273e83714100c19fc95dabafdd
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-05 16:56:39 -0500

    .

--- handle.hpp
+++ handle.hpp
@@ -6,17 +6,17 @@ template
 	template<typename> typename AcquirePolicy,
 	template<typename> typename ReleasePolicy
 >
-class RAII
+class handle
 {
 public:
-	explicit RAII(T h) : m_handle(h) { AcquirePolicy<T>::Execute(m_handle); }
-	RAII(const RAII&) = delete;
-	RAII(RAII&&) = default;
-	RAII& operator = (const RAII&) = delete;
-	RAII& operator = (RAII&&) = default;
-	~RAII() { ReleasePolicy<T>::Execute(m_handle); }
+	explicit handle(T h) : m_handle(h) { AcquirePolicy<T>::Execute(m_handle); }
+	handle(const handle&) = delete;
+	handle(handle&&) = default;
+	handle& operator = (const handle&) = delete;
+	handle& operator = (handle&&) = default;
+	~handle() { ReleasePolicy<T>::Execute(m_handle); }
 
-	operator T() const { return m_handle; }
+	operator T () const { return m_handle; }
 
 private:
 	T m_handle;
@@ -27,5 +27,5 @@ template<typename T> struct NoOpPolicy { static void Execute(T) noexcept {} };
 template<typename T> struct PointerReleasePolicy { static void Execute(T ptr) noexcept { delete ptr; } };
 template<typename T> struct ArrayReleasePolicy { static void Execute(T ptr) noexcept { delete[] ptr; } };
 
-template<typename T> using arr_ptr_handle_t = RAII<T*, NoOpPolicy, ArrayReleasePolicy>;
-template<typename T> using ptr_handle_t = RAII<T*, NoOpPolicy, PointerReleasePolicy>;
+template<typename T> using arr_ptr_handle_t = handle<T*, NoOpPolicy, ArrayReleasePolicy>;
+template<typename T> using ptr_handle_t = handle<T*, NoOpPolicy, PointerReleasePolicy>;
