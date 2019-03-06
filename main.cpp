e0671f3ca0337ec3ca3d2f56085dce98ff6777ab
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-05 23:46:43 -0500

    Catch2 benchmark of STL vs PSTL

--- main.cpp
+++ main.cpp
@@ -1,40 +1,33 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 #include <vector>
-#include <chrono>
 #include <random>
 #include <algorithm>
 #include <pstl/execution>
 #include <pstl/algorithm>
 using namespace std;
-using namespace chrono;
 using namespace pstl;
 
-const unsigned long long COUNT = 10'000'000;
+const unsigned long long COUNT = 100'000'000;
 
-int main()
+TEST_CASE("STL vs PSTL", "[benchmark]")
 {
 	random_device rd;
 	mt19937 mt(rd());
 
 	vector<int> data(COUNT);
 
-	auto start = high_resolution_clock::now();
+	BENCHMARK("STL")
+	{
+		generate(data.begin(), data.end(), mt);
+		sort(data.begin(), data.end());
+		is_sorted(data.begin(), data.end());
+	}
 
-	generate(data.begin(), data.end(), mt);
-	sort(data.begin(), data.end());
-	is_sorted(data.begin(), data.end());
-
-	auto end = high_resolution_clock::now();
-	auto duration = duration_cast<milliseconds>(end - start);
-	cout << "STL duration = " << duration.count() / 1000.f << "s" << endl;
-
-	start = high_resolution_clock::now();
-
-	generate(pstl::execution::par_unseq, data.begin(), data.end(), mt);
-	sort(pstl::execution::par_unseq, data.begin(), data.end());
-	is_sorted(pstl::execution::par_unseq, data.begin(), data.end());
-
-	end = high_resolution_clock::now();
-	duration = duration_cast<milliseconds>(end - start);
-	cout << "PSTL duration = " << duration.count() / 1000.f << "s" << endl;
+	BENCHMARK("PSTL")
+	{
+		generate(pstl::execution::par_unseq, data.begin(), data.end(), mt);
+		sort(pstl::execution::par_unseq, data.begin(), data.end());
+		is_sorted(pstl::execution::par_unseq, data.begin(), data.end());
+	}
 }
