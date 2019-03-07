ef281c618e443858da69b18344b0440b70e50156
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-07 09:08:23 -0500

    Update pool and random tests to use Catch2 for benchmark

--- pool_test.cpp
+++ pool_test.cpp
@@ -1,44 +1,37 @@
-#include <iostream>
-#include <chrono>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 #include <cstdlib>
 #include "pool.hpp"
 using namespace std;
-using namespace chrono;
 
 const int COUNT = 1'000'000;
 const int REPS = 10;
 
-int main()
+TEST_CASE("simple_thread_pool vs thread_pool", "[benchmark]")
 {
-	srand(0);
-	auto start = high_resolution_clock::now();
+	BENCHMARK("simple_thread_pool")
 	{
+		srand(0);
 		simple_thread_pool tp;
 		for(int i = 0; i < COUNT; ++i)
-		tp.enqueue_work([i]() {
-			int x;
-			int reps = REPS + (REPS * (rand() % 5));
-			for(int n = 0; n < reps; ++n)
-			x = i + rand();
-		});
+			tp.enqueue_work([i]()
+			{
+				int reps = REPS + (REPS * (rand() % 5));
+				for(int n = 0; n < reps; ++n)
+					int x = i + rand();
+			});
 	}
-	auto end = high_resolution_clock::now();
-	auto duration = duration_cast<milliseconds>(end - start);
-	cout << "simple_thread_pool duration = " << duration.count() / 1000.f << " s" << endl;
-	
-	srand(0);
-	start = high_resolution_clock::now();
+
+	BENCHMARK("thread_pool")
 	{
+		srand(0);
 		thread_pool tp;
 		for(int i = 0; i < COUNT; ++i)
-		tp.enqueue_work([i]() {
-			int x;
-			int reps = REPS + (REPS * (rand() % 5));
-			for(int n = 0; n < reps; ++n)
-			x = i + rand();
-		});
+			tp.enqueue_work([i]()
+			{
+				int reps = REPS + (REPS * (rand() % 5));
+				for(int n = 0; n < reps; ++n)
+					int x = i + rand();
+			});
 	}
-	end = high_resolution_clock::now();
-	duration = duration_cast<milliseconds>(end - start);
-	cout << "thread_pool duration = " << duration.count() / 1000.f << " s" << endl;
 }
