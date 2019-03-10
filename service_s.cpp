62a1f023d53bc1574fa8620696a504f69e8a0441
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-10 10:23:42 -0400

    Simplified thrift server

--- service_s.cpp
+++ service_s.cpp
@@ -23,32 +23,10 @@ public:
 	void async_call() override { cout << "async_call()" << endl; }
 };
 
-class ServiceCloneFactory : virtual public ServiceIfFactory
-{
-public:
-	ServiceCloneFactory() = default;
-
-	ServiceIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo) override
-	{
-		auto sock = std::dynamic_pointer_cast<TSocket>(connInfo.transport);
-		cout << "Incoming connection" << endl;
-		cout << "\tSocketInfo : " << sock->getSocketInfo() << endl;
-		cout << "\tPeerHost   : " << sock->getPeerHost() << endl;
-		cout << "\tPeerAddress: " << sock->getPeerAddress() << endl;
-		cout << "\tPeerPort   : " << sock->getPeerPort() << endl;
-		return new ServiceHandler;
-	}
-
-	void releaseHandler(ServiceIf* handler) override
-	{
-		delete handler;
-	}
-};
-
 int main(int argc, char** argv)
 {
 	TSimpleServer server(
-		std::make_shared<ServiceProcessorFactory>(std::make_shared<ServiceCloneFactory>()),
+		std::make_shared<ServiceProcessor>(std::make_shared<ServiceHandler>()),
 		std::make_shared<TServerSocket>(9090),
 		std::make_shared<TBufferedTransportFactory>(),
 		std::make_shared<TBinaryProtocolFactory>());
