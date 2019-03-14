a4fb6dcc9c1ea6cc06975225474b535b0bb3caa0
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-14 10:10:20 -0400

    Database example with SOCI

--- src/database.cpp
+++ src/database.cpp
@@ -1,7 +1,38 @@
+#include <iostream>
+#include <string>
+#include <exception>
 #include <soci/soci.h>
-#include <soci/sqlite3/soci-sqlite3.h>
+#include <soci/mysql/soci-mysql.h>
+using namespace soci;
+using namespace std;
 
-int main(int argc, char** argv)
+int main()
 {
-	return 1;
+	try
+	{
+		session sql(mysql, "host=localhost user=root password=''");
+
+		sql << "CREATE DATABASE blog";
+		sql << "USE blog";
+		sql << "CREATE TABLE people (FirstName TEXT, LastName TEXT, DOB INT, EMail TEXT)";
+		sql << "INSERT INTO people (FirstName, LastName, DOB, EMail) VALUES ('Martin', 'Vorbrodt', 19800830, 'martin@vorbrodt.blog')";
+
+		int count{};
+		sql << "SELECT COUNT(*) FROM people", into(count);
+		cout << "Table 'people' has " << count << " row(s)" << endl;
+
+		row r;
+		sql << "SELECT * FROM people", into(r);
+		string FirstName, LastName, EMail;
+		int DOB;
+		r >> FirstName >> LastName >> DOB >> EMail;
+		cout << FirstName << ", " << LastName << ", " << DOB << ", " << EMail << endl;
+
+		sql << "DROP TABLE people";
+		sql << "DROP DATABASE blog";
+	}
+	catch (exception& e)
+	{
+		cerr << e.what() << endl;
+	}
 }
