12524bb87a90546d0e740a53fe9ab6f73968e51a
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-15 18:56:55 -0400

    Better cpu timer example

--- src/cpu_timer.cpp
+++ src/cpu_timer.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <thread>
 #include <cmath>
 #include <boost/timer/timer.hpp>
@@ -10,27 +9,23 @@ int main()
 {
 	auto_cpu_timer program_timer(3);
 
-	auto proc = [](long work)
+	auto procUser = [](long work)
 	{
-		cpu_timer thread_timer;
-		thread_timer.start();
-
 		for (long i = 0; i < work; ++i)
 			sqrt(123.456L);
-
-		thread_timer.stop();
-		cout << "Thread timer:" << thread_timer.format(3);
 	};
 
-	thread t1(proc, 10000000);
-	thread t2(proc, 100000000);
-	thread t3(proc, 1000000000);
+	auto procSystem = [](long work)
+	{
+		for (long i = 0; i < work; ++i)
+			thread([](){}).detach();
+	};
+
+	thread t1(procUser, 1000000000);
+	thread t2(procSystem, 100000);
 
 	t1.join();
 	t2.join();
-	t3.join();
-
-	cout << "Program timer:";
 
 	return 0;
 }
