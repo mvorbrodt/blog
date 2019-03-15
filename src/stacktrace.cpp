a4b5d61fe2eb7e58920c680d5766acde639bb5d5
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-15 12:59:02 -0400

    Update stacktrace example

--- src/stacktrace.cpp
+++ src/stacktrace.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <future>
 #define BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED
 #include <boost/stacktrace.hpp>
 
@@ -11,11 +12,14 @@ void f3();
 
 void f1() { f2(); }
 void f2() { f3(); }
-void f3() { cout << stacktrace() << endl; }
+void f3() { cout << stacktrace() << endl << endl; }
 
 int main(int argc, char** argv)
 {
 	f1();
+
+	auto f = async(launch::async, []() { cout << stacktrace() << endl << endl; });
+	f.get();
 
 	return 1;
 }
