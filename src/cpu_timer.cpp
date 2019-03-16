f56968a1af50cd73c2a02cb89e86a70db480703f
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-16 10:26:19 -0400

    Updated CPU timer example

--- src/cpu_timer.cpp
+++ src/cpu_timer.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <thread>
 #include <cmath>
+#include <cstdlib>
 #include <boost/timer/timer.hpp>
 
 using namespace std;
@@ -7,6 +9,8 @@ using namespace boost::timer;
 
 int main()
 {
+	srand((unsigned int)time(NULL));
+
 	auto_cpu_timer program_timer(3);
 
 	auto procUser = [](long work)
@@ -21,11 +25,27 @@ int main()
 			thread([](){}).detach();
 	};
 
+	auto procTimer = [](long work)
+	{
+		cpu_timer timer;
+		timer.start();
+
+		for(long i = 0; i < work; ++i)
+			rand();
+
+		timer.stop();
+		cout << "Thread timer:" << timer.format(3);
+	};
+
 	thread t1(procUser, 1000000000);
 	thread t2(procSystem, 100000);
+	thread t3(procTimer, 100000000);
 
 	t1.join();
 	t2.join();
+	t3.join();
 
-	return 0;
+	cout << "Program timer:";
+
+	return 1;
 }
