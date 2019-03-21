81bafa4985ca011298ecdc3e95f2a0481e0cc3be
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-21 18:58:21 -0400

    LZ4 compression/decompression example

---
+++ src/compression.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <lz4.h>
+
+using namespace std;
+
+using buffer = vector<char>;
+
+void lz4_compress(const buffer& in, buffer& out)
+{
+	auto rv = LZ4_compress_default(in.data(), out.data(), in.size(), out.size());
+	if(rv < 1) cerr << "Something went wrong!" << endl;
+	else out.resize(rv);
+}
+
+void lz4_decompress(const buffer& in, buffer& out)
+{
+	auto rv = LZ4_decompress_safe(in.data(), out.data(), in.size(), out.size());
+	if(rv < 1) cerr << "Something went wrong!" << endl;
+	else out.resize(rv);
+}
+
+int main(int argc, char** argv)
+{
+	string s{"This is our data that will be repeated 1000 times and compressed :o)"};
+	buffer data;
+	for(int copies = 0; copies < 1000; ++copies)
+		copy(begin(s), end(s), back_inserter(data));
+	buffer compressed(data.size()), decompressed(data.size());
+
+	lz4_compress(data, compressed);
+	cout << "LZ4 compress, bytes in: " << data.size() << ", bytes out: " << compressed.size() << endl;
+
+	lz4_decompress(compressed, decompressed);
+	cout << "LZ4 decompress, bytes in: " << compressed.size() << ", bytes out: " << decompressed.size() << endl;
+
+	if(data != decompressed) cerr << "Oh snap! Data mismatch!" << endl;
+	else cout << "Decompressed data matches original :o)" << endl;
+
+	return 1;
+}
