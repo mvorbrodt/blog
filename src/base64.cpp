920f9265ff763e8a89c6ee6385162dd6aec3dbad
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-22 20:29:40 -0400

    Beautify and turn base64 into modern C++

--- src/base64.cpp
+++ src/base64.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
 #include "base64.hpp"
+#include "ascii_escape_code.hpp"
 
 using namespace std;
+using namespace ascii_escape_code;
 
 int main(int argc, char** argv)
 {
-	string s{"Vorbrodt's C++ Blog"};
-	vector<BYTE> data(begin(s), end(s));
+	string s
+	{
+		"Man is distinguished, not only by his reason, but by this singular passion from other animals, "
+		"which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable "
+		"generation of knowledge, exceeds the short vehemence of any carnal pleasure. ~ Thomas Hobbes' Leviathan"
+	};
+	vector<base64::byte> data(begin(s), end(s));
 
-	cout << "Input  : " << s << endl;
+	cout << bold << bright_red << "Input: " << reset << italic << s << reset << endl << endl;
 
 	auto encoded = base64::encode(data);
-	cout << "Encoded: " << encoded << endl;
+	cout << bold << bright_red << "Encoded: " << reset << encoded << endl << endl;
 
 	auto decoded = base64::decode(encoded);
 	string s2(begin(decoded), end(decoded));
 
-	cout << "Decoded: " << s2 << endl;
+	cout << bold << bright_red << "Decoded: " << reset << italic << s2 << reset << endl << endl;
+
+	if(data != decoded) cerr << "Oh snap! Data mismatch!" << endl;
+	else cout << "Decoded data matches original :o)" << endl;
 
 	return 1;
 }
--- src/base64.hpp
+++ src/base64.hpp
@@ -5,104 +5,107 @@
 #include <stdexcept>
 #include <cstdint>
 
-#define TCHAR char
-#define TEXT(x) x
-#define BYTE unsigned char
-
 namespace base64
 {
-	inline static const TCHAR encodeLookup[] = TEXT("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
-	inline static const TCHAR padCharacter = TEXT('=');
+	inline static const char kEncodeLookup[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	inline static const char kPadCharacter = '=';
 
-	inline std::basic_string<TCHAR> encode(std::vector<BYTE> inputBuffer)
+	using byte = std::uint8_t;
+
+	inline std::string encode(const std::vector<byte>& input)
 	{
-		std::basic_string<TCHAR> encodedString;
-		encodedString.reserve(((inputBuffer.size()/3) + (inputBuffer.size() % 3 > 0)) * 4);
-		uint32_t temp;
-		std::vector<BYTE>::iterator cursor = inputBuffer.begin();
-		for(size_t idx = 0; idx < inputBuffer.size()/3; idx++)
+		std::string encoded;
+		encoded.reserve(((input.size() / 3) + (input.size() % 3 > 0)) * 4);
+
+		std::uint32_t temp{};
+		auto it = input.begin();
+
+		for(std::size_t i = 0; i < input.size() / 3; ++i)
 		{
-			temp  = (*cursor++) << 16;
-			temp += (*cursor++) << 8;
-			temp += (*cursor++);
-			encodedString.append(1,encodeLookup[(temp & 0x00FC0000) >> 18]);
-			encodedString.append(1,encodeLookup[(temp & 0x0003F000) >> 12]);
-			encodedString.append(1,encodeLookup[(temp & 0x00000FC0) >> 6 ]);
-			encodedString.append(1,encodeLookup[(temp & 0x0000003F)      ]);
+			temp  = (*it++) << 16;
+			temp += (*it++) << 8;
+			temp += (*it++);
+			encoded.append(1, kEncodeLookup[(temp & 0x00FC0000) >> 18]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0003F000) >> 12]);
+			encoded.append(1, kEncodeLookup[(temp & 0x00000FC0) >> 6 ]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0000003F)      ]);
 		}
-		switch(inputBuffer.size() % 3)
+
+		switch(input.size() % 3)
 		{
 		case 1:
-			temp  = (*cursor++) << 16;
-			encodedString.append(1,encodeLookup[(temp & 0x00FC0000) >> 18]);
-			encodedString.append(1,encodeLookup[(temp & 0x0003F000) >> 12]);
-			encodedString.append(2,padCharacter);
+			temp = (*it++) << 16;
+			encoded.append(1, kEncodeLookup[(temp & 0x00FC0000) >> 18]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0003F000) >> 12]);
+			encoded.append(2, kPadCharacter);
 			break;
 		case 2:
-			temp  = (*cursor++) << 16;
-			temp += (*cursor++) << 8;
-			encodedString.append(1,encodeLookup[(temp & 0x00FC0000) >> 18]);
-			encodedString.append(1,encodeLookup[(temp & 0x0003F000) >> 12]);
-			encodedString.append(1,encodeLookup[(temp & 0x00000FC0) >> 6 ]);
-			encodedString.append(1,padCharacter);
+			temp  = (*it++) << 16;
+			temp += (*it++) << 8;
+			encoded.append(1, kEncodeLookup[(temp & 0x00FC0000) >> 18]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0003F000) >> 12]);
+			encoded.append(1, kEncodeLookup[(temp & 0x00000FC0) >> 6 ]);
+			encoded.append(1, kPadCharacter);
 			break;
 		}
-		return encodedString;
+
+		return encoded;
 	}
 
-	std::vector<BYTE> decode(const std::basic_string<TCHAR>& input)
+	std::vector<byte> decode(const std::string& input)
 	{
-		if (input.length() % 4)
-			throw std::runtime_error("Non-Valid base64!");
-		size_t padding = 0;
-		if (input.length())
+		if(input.length() % 4)
+			throw std::runtime_error("Invalid base64 length!");
+
+		std::size_t padding{};
+
+		if(input.length())
 		{
-			if (input[input.length()-1] == padCharacter)
-				padding++;
-			if (input[input.length()-2] == padCharacter)
-				padding++;
+			if(input[input.length() - 1] == kPadCharacter) padding++;
+			if(input[input.length() - 2] == kPadCharacter) padding++;
 		}
-		std::vector<BYTE> decodedBytes;
-		decodedBytes.reserve(((input.length()/4)*3) - padding);
-		uint32_t temp=0;
-		std::basic_string<TCHAR>::const_iterator cursor = input.begin();
-		while (cursor < input.end())
+
+		std::vector<byte> decoded;
+		decoded.reserve(((input.length() / 4) * 3) - padding);
+
+		std::uint32_t temp{};
+		auto it = input.begin();
+
+		while(it < input.end())
 		{
-			for (size_t quantumPosition = 0; quantumPosition < 4; quantumPosition++)
+			for(std::size_t i = 0; i < 4; ++i)
 			{
 				temp <<= 6;
-				if       (*cursor >= 0x41 && *cursor <= 0x5A)
-					temp |= *cursor - 0x41;
-				else if  (*cursor >= 0x61 && *cursor <= 0x7A)
-					temp |= *cursor - 0x47;
-				else if  (*cursor >= 0x30 && *cursor <= 0x39)
-					temp |= *cursor + 0x04;
-				else if  (*cursor == 0x2B)
-					temp |= 0x3E;
-				else if  (*cursor == 0x2F)
-					temp |= 0x3F;
-				else if  (*cursor == padCharacter)
+				if     (*it >= 0x41 && *it <= 0x5A) temp |= *it - 0x41;
+				else if(*it >= 0x61 && *it <= 0x7A) temp |= *it - 0x47;
+				else if(*it >= 0x30 && *it <= 0x39) temp |= *it + 0x04;
+				else if(*it == 0x2B)                temp |= 0x3E;
+				else if(*it == 0x2F)                temp |= 0x3F;
+				else if(*it == kPadCharacter)
 				{
-					switch( input.end() - cursor )
+					switch(input.end() - it)
 					{
 					case 1:
-						decodedBytes.push_back((temp >> 16) & 0x000000FF);
-						decodedBytes.push_back((temp >> 8 ) & 0x000000FF);
-						return decodedBytes;
+						decoded.push_back((temp >> 16) & 0x000000FF);
+						decoded.push_back((temp >> 8 ) & 0x000000FF);
+						return decoded;
 					case 2:
-						decodedBytes.push_back((temp >> 10) & 0x000000FF);
-						return decodedBytes;
+						decoded.push_back((temp >> 10) & 0x000000FF);
+						return decoded;
 					default:
-						throw std::runtime_error("Invalid Padding in Base 64!");
+						throw std::runtime_error("Invalid padding in base64!");
 					}
-				}  else
-					throw std::runtime_error("Non-Valid Character in Base 64!");
-				cursor++;
+				}
+				else throw std::runtime_error("Invalid character in base64!");
+
+				++it;
 			}
-			decodedBytes.push_back((temp >> 16) & 0x000000FF);
-			decodedBytes.push_back((temp >> 8 ) & 0x000000FF);
-			decodedBytes.push_back((temp      ) & 0x000000FF);
+
+			decoded.push_back((temp >> 16) & 0x000000FF);
+			decoded.push_back((temp >> 8 ) & 0x000000FF);
+			decoded.push_back((temp      ) & 0x000000FF);
 		}
-		return decodedBytes;
+
+		return decoded;
 	}
 }
