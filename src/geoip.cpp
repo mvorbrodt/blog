fd49624b2e6d99849220651493bd9329cf9513f0
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-28 09:55:54 -0400

    Update GeoIP example

--- src/geoip.cpp
+++ src/geoip.cpp
@@ -5,26 +5,35 @@
 #include <curlpp/Options.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include "ascii_escape_code.hpp"
 
 using namespace std;
 using namespace boost::property_tree;
+using namespace ascii_escape_code;
 
 int main()
 {
-	curlpp::Cleanup clean;
-
-	curlpp::Easy request;
-	std::stringstream response;
-
-	request.setOpt(new curlpp::options::Url("http://ip-api.com/json/vorbrodt.blog"));
-	request.setOpt(new curlpp::options::WriteStream(&response));
-	request.perform();
-
-	ptree data;
-	read_json(response, data);
-
-	for(auto& it : data)
+	try
 	{
-		cout << it.first << " = " << it.second.data() << endl;
+		curlpp::Cleanup clean;
+
+		curlpp::Easy request;
+		std::stringstream response;
+
+		request.setOpt(new curlpp::options::Url("http://ip-api.com/json/vorbrodt.blog"));
+		request.setOpt(new curlpp::options::WriteStream(&response));
+		request.perform();
+
+		ptree data;
+		read_json(response, data);
+
+		for(auto& it : data)
+		{
+			cout << it.first << " = " << it.second.data() << endl;
+		}
+	}
+	catch(exception& e)
+	{
+		cerr << bold << bright_red << slow_blink << e.what() << reset << endl;
 	}
 }
