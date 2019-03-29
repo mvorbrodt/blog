e72fd986b68e554bc5bc68730a3117fb15926955
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-29 19:17:05 -0400

    Update GeoIP example

--- src/geoip.cpp
+++ src/geoip.cpp
@@ -7,28 +7,28 @@
 #include <boost/property_tree/json_parser.hpp>
 
 using namespace std;
-using namespace boost::property_tree;
 
 int main()
 {
 	try
 	{
-		curlpp::Cleanup clean;
+		curlpp::Cleanup cURLppStartStop;
 
 		curlpp::Easy request;
 		std::stringstream response;
 
-		request.setOpt(new curlpp::options::Url("http://ip-api.com/json/vorbrodt.blog"));
-		request.setOpt(new curlpp::options::WriteStream(&response));
+		request.setOpt(curlpp::options::Verbose(true));
+		request.setOpt(curlpp::options::WriteStream(&response));
+		request.setOpt(curlpp::options::Url("http://ip-api.com/json/vorbrodt.blog"));
+		request.setOpt(curlpp::options::Port(80));
+
 		request.perform();
 
-		ptree data;
-		read_json(response, data);
+		boost::property_tree::ptree data;
+		boost::property_tree::read_json(response, data);
 
 		for(auto& it : data)
-		{
 			cout << it.first << " = " << it.second.data() << endl;
-		}
 	}
 	catch(exception& e)
 	{
