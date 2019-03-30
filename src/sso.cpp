126803ea48a95b56f2b176a8585da3821565980f
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-03-30 19:16:30 -0400

    Update SSO example

--- src/sso.cpp
+++ src/sso.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main()
 {
+	auto size = sizeof(string);
 	auto capacity = string().capacity();
 	auto small = string(capacity, '*');
 	auto big = string(capacity + 1, '*');
 
+	cout << "sizeof  : " << size << endl;
 	cout << "Capacity: " << capacity << endl;
 	cout << "Small   : " << small.capacity() << endl;
 	cout << "Big     : " << big.capacity() << endl;
