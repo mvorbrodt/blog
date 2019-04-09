2b9e08755d1b29a05df5bccec6ad9a2a75e72420
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-04-09 15:56:37 -0400

    Update multihash example

--- src/multi_hash.cpp
+++ src/multi_hash.cpp
@@ -4,22 +4,51 @@
 
 using namespace std;
 
-int main()
+void arr()
 {
 	string s1 = "Vorbrodt's C++ Blog";
 	string s2 = "Vorbrodt's C++ Blog";
 	string s3 = "https://vorbrodt.blog";
 
-	auto [s1h1, s1h2, s1h3] = hashN<3>(s1);
-	auto [s2h1, s2h2, s2h3] = hashN<3>(s2);
-	auto [s3h1, s3h2, s3h3] = hashN<3>(s3);
+	auto h1 = hashN(s1, 3);
+	auto h2 = hashN(s2, 3);
+	auto h3 = hashN(s3, 3);
 
 	cout << "HashN('" << s1 << "'):" << endl;
-	cout << s1h1 << ", " << s1h2 << ", " << s1h3 << endl << endl;
+	for(auto it : h1) cout << it << endl;
+	cout << endl;
 
 	cout << "HashN('" << s2 << "'):" << endl;
-	cout << s2h1 << ", " << s2h2 << ", " << s2h3 << endl << endl;
+	for(auto it : h2) cout << it << endl;
+	cout << endl;
 
 	cout << "HashN('" << s3 << "'):" << endl;
-	cout << s3h1 << ", " << s3h2 << ", " << s3h3 << endl << endl;
+	for(auto it : h3) cout << it << endl;
+	cout << endl;
+}
+
+void temp()
+{
+	string s1 = "Vorbrodt's C++ Blog";
+	string s2 = "Vorbrodt's C++ Blog";
+	string s3 = "https://vorbrodt.blog";
+
+	auto [s1h1, s1h2, s1h3] = hashNT<3>(s1);
+	auto [s2h1, s2h2, s2h3] = hashNT<3>(s2);
+	auto [s3h1, s3h2, s3h3] = hashNT<3>(s3);
+
+	cout << "HashNT('" << s1 << "'):" << endl;
+	cout << s1h1 << endl << s1h2 << endl << s1h3 << endl << endl;
+
+	cout << "HashNT('" << s2 << "'):" << endl;
+	cout << s2h1 << endl << s2h2 << endl << s2h3 << endl << endl;
+
+	cout << "HashNT('" << s3 << "'):" << endl;
+	cout << s3h1 << endl << s3h2 << endl << s3h3 << endl << endl;
+}
+
+int main()
+{
+	arr();
+	temp();
 }
--- src/multi_hash.hpp
+++ src/multi_hash.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
 #include <array>
+#include <vector>
 #include <random>
 #include <algorithm>
 #include <functional>
 
-template<size_t N, typename T>
-auto hashN(const T& key) -> std::array<size_t, N>
+template<typename T>
+auto hashN(const T& key, std::size_t N) -> std::vector<std::size_t>
 {
 	std::minstd_rand0 rng(std::hash<T>{}(key));
-	std::array<size_t, N> hashes = {};
-	std::generate(begin(hashes), end(hashes), rng);
+	std::vector<std::size_t> hashes(N);
+	std::generate(std::begin(hashes), std::end(hashes), rng);
+	return hashes;
+}
+
+template<std::size_t N, typename T>
+auto hashNT(const T& key) -> std::array<std::size_t, N>
+{
+	std::minstd_rand0 rng(std::hash<T>{}(key));
+	std::array<std::size_t, N> hashes{};
+	std::generate(std::begin(hashes), std::end(hashes), rng);
 	return hashes;
 }
