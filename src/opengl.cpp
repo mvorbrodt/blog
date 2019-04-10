012e61403b05190ab06f5c1f9143bb49a95fa275
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-04-10 12:07:22 -0400

    Use GLUT instead

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#ifdef __APPLE__
+#include <GL/glew.h>
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
@@ -9,50 +7,44 @@
 
 using namespace std;
 
+void init()
+{
+	glewExperimental=true;
+	glewInit();
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(60.0, 640.0 / 480.0, 1.0, 100.0);
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 100.0);
+}
+
+void draw()
+{
+	static float rotation{};
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -3.0);
+	glRotatef(rotation, 0.0, 1.0, 0.0);
+	glutWireTeapot(1.0);
+	glutSwapBuffers();
+	rotation += 0.5;
+}
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-
-	if (!glfwInit())
-		return -1;
-
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	glfwSwapInterval(1);
-
-	cout << "Vendor          : " << glGetString(GL_VENDOR) << endl;
-	cout << "Renderer        : " << glGetString(GL_RENDERER) << endl;
-	cout << "Version         : " << glGetString(GL_VERSION) << endl;
-	cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
-	cout << "Extensions      : " << glGetString(GL_EXTENSIONS) << endl;
-
-	glMatrixMode(GL_PROJECTION);
-	gluPerspective(60.0, 640.0 / 480.0, 1.0, 100.0);
-
-	float rotation = 0.0;
-	while (!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glTranslatef(0.0, 0.0, -3.0);
-		glRotatef(rotation, 0.0, 1.0, 0.0);
-
-		glutWireTeapot(1.0);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-		rotation += 0.5;
-	}
-
-	glfwTerminate();
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(640, 480);
+	glutCreateWindow("Hello OpenGL");
+	init();
+	glutIdleFunc(draw);
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(draw);
+	glutMainLoop();
 }
