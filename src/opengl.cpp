d477fac8ae96ab223b2db905d7f58e72b17e0bcf
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-04-10 09:01:52 -0400

    OpenGL example

---
+++ src/opengl.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <GLUT/glut.h>
+
+using namespace std;
+
+int main()
+{
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwSwapInterval(1);
+
+	cout << "Vendor          : " << glGetString(GL_VENDOR) << endl;
+	cout << "Renderer        : " << glGetString(GL_RENDERER) << endl;
+	cout << "Version         : " << glGetString(GL_VERSION) << endl;
+	cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+	cout << "Extensions      : " << glGetString(GL_EXTENSIONS) << endl;
+
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(60.0, 640.0 / 480.0, 1.0, 100.0);
+
+	float rotation = 0.0;
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glTranslatef(0.0, 0.0, -3.0);
+		glRotatef(rotation, 0.0, 1.0, 0.0);
+
+		glutWireTeapot(1.0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		rotation += 0.5;
+	}
+
+	glfwTerminate();
+}
