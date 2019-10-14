74c0fa17f56416cdd546b564b4b7d0e9ca58ec2b
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-10-14 16:29:41 -0400

    Testing few things...

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <iomanip>
 #include <memory>
+#include <exception>
 
 using namespace std;
+
+struct S
+{
+	S() : S(1) {}
+	S(int) { throw runtime_error("S(int)"); }
+	~S() { cout << "~S()" << endl; }
+};
 
 int main()
 {
 	auto p = unique_ptr<int[]>(new int[1]);
+
+	try
+	{
+		S s;
+	}
+	catch(exception& e)
+	{
+		cout << e.what() << endl;
+	}
 }
