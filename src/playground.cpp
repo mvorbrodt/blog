82de5586cc178f1758c9ab01592b5816e6b24570
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-10-15 11:28:32 -0400

    Chaining constructors and the destructor test

--- src/playground.cpp
+++ src/playground.cpp
@@ -7,17 +7,28 @@
 
 using namespace std;
 
+struct M
+{
+	M(const char* s) : m_s(s) { cout << "M::M(m_s = '" << m_s << "')" << endl; }
+	~M() { cout << "M::~M(m_s = '" << m_s << "')" << endl; }
+
+	string m_s;
+};
+
 struct S
 {
-	S() : S(1) {}
-	S(int) { throw runtime_error("S(int)"); }
-	~S() { cout << "~S()" << endl; }
+	S() : S(2019) { cout << "S::S() throws..." << endl; throw runtime_error("BOOM!"); }
+	S(int i) : S("C++") { cout << "S::S(int i = '" << i << "')" << endl; }
+	S(const char* s) : m_pM(make_unique<M>("by pointer")), m_vM("by value")
+		{ cout << "S::S(const char* s = '" << s << "')" << endl; }
+	~S() { cout << "S::~S() cleans up..." << endl; }
+
+	M m_vM;
+	unique_ptr<M> m_pM = nullptr;
 };
 
 int main()
 {
-	auto p = unique_ptr<int[]>(new int[1]);
-
 	try
 	{
 		S s;
