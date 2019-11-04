699e84a8a7e94d37aacb89ee1d7f3d00c19db5e1
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-11-04 18:37:33 -0500

    Update to ranges library example

--- src/ranges.cpp
+++ src/ranges.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <range/v3/range.hpp>
 #include <range/v3/view.hpp>
 
 using namespace std;
+namespace r = ranges;
+namespace rv = ranges::views;
 
 int main(int argc, char** argv)
 {
-	std::vector<int> numbers = { 1, 2, 3 ,4, 5 };
+	using Ints = vector<int>;
+	Ints numbers = { 1, 2, 3, 4, 5 };
 
 	auto evenNumbers = numbers
-		| ranges::view::filter([](int n){ return n % 2 == 0; })
-		| ranges::view::transform([](int n) { return n * 2; });
+		| rv::filter([](int n){ return n % 2 == 0; })
+		| rv::transform([](int n) { return n * 2; });
 
 	for(auto _ : evenNumbers)
 		cout << _ << " ";
 	cout << endl;
+
+	auto xs = rv::ints(1, 11) // Make int range, 1 to 10... 11 because stl ranges exclude last element
+		| rv::transform([](auto i) { return std::make_pair(i, i * i); }) // transform each int in range
+		| r::to<vector>(); // convert generated range to standard vector
+
+	for_each(begin(xs), end(xs), [](auto& p) { cout << p.first << ", " << p.second << endl; });
 
 	return 1;
 }
