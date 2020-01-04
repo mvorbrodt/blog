4b82b26e90c74f70526ff394c50b388213ebdd98
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2020-01-04 16:40:41 -0500

    Build script for Windows

--- run_cmake.bat
+++ run_cmake.bat
@@ -1,2 +1,8 @@
 @echo off
+copy /Y CMakeLists.win.txt CMakeLists.txt
+copy /Y src\CMakeLists.win.txt src\CMakeLists.txt
 cmake -DCMAKE_TOOLCHAIN_FILE=C:\Code\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows .
+msbuild -m blog.sln -t:Clean
+msbuild -m blog.sln -t:Rebuild -p:Configuration=Release
+git checkout CMakeLists.txt
+git checkout src\CMakeLists.txt
