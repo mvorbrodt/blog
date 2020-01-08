22d8391f5d22fd74f9608a5df322870b29a2f43c
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2020-01-08 14:04:46 -0500

    Property work in progress...

--- src/property.cpp
+++ src/property.cpp
@@ -8,82 +8,45 @@ using namespace std;
 
 int main()
 {
-	property<int*> pi = new int[2];
-	property<int> pi2;
-	cout << pi << endl;
-	*pi++ = 1;
-	cout << pi << endl;
-	*pi++ = 2;
-	cout << pi << endl;
-	pi -= 1;
-	--pi;
-	pi += pi2;
-	cout << pi << endl;
-	cout << pi[0] << ", " << pi[1] << endl << endl;
-
-	property<T> abc;
-	cout << abc.invoke(&T::get_instance_number) << endl << endl;
-	abc.invoke(&T::foo);
-
-	property<vector<int>> pv1 = make_property<vector<int>>(1, 2, 3, 4, 5);
-	pv1[1] = 1;
-	pv1.invoke(&vector<int>::size);
-	((vector<int>)pv1).size();
-
-	using up_t = std::unique_ptr<T>;
-	using sp_t = std::shared_ptr<T>;
-
-	property<up_t> up_p1 = make_unique<T>();
-	up_p1->get_instance_number();
-
-	property<sp_t> sp_p2 = make_property<sp_t>(new T);
-	sp_p2->get_instance_number();
-
-	return 1;
-
-	auto p1 = make_property<int>(44);
-	auto p2 = make_property<int>(66);
+	// W/ PRIMITIVE TYPES
+	auto p1 = make_property<int>(1);
+	auto p2 = make_property<int>(2);
 
 	property<const float> p3(p2);
-	const property<double> p4(p3), p5(p2);
+	const property<double> p4{p3};
 
-	p1.add_update_event([](auto p) { cout << "p1 updated with value: " << p << endl; });
-	p2.add_update_event([](auto p) { cout << "p2 updated with value: " << p << endl; });
-	p3.add_update_event([](auto p) { cout << "p3 updated with value: " << p << endl; });
+	p1.add_update_event([](auto p) { cout << "~~~ p1 updated with value: " << p << endl; });
 
-	p2 = p1;
-	p2 = move(p1);
-	p2 = p3;
-	p2 = move(p3);
+	p1 = p2;
+	p1 = p3;
+	++p1;
+	p1 *= p4;
 
-	int x = 123;
-	p1 = 123;
-	p1 = 123.f;
-	p1 = x;
+	// W/ COMPLEX TYPES
+	property<Q> t1;
+	property<T> t2(std::move(t1));
 
-	const property<int> cp1(12345);
-	property<int> p6(cp1);
-	property<float> p7(cp1);
-	property<float> p8(std::move(p1));
-	property<float> p9(std::move(p8));
-	int xyz = cp1;
+	Q q;
+	T t(std::move(q));
+	t = std::move(q);
+	//property<T> t3(std::move(t1));
+	//t2 = t3;
+	//t3 = std::move(t1);
+	/*auto p5 = make_property<T>(1, 2, 3);
+	auto p6 = make_property<Q>(4, 5, 6);
 
-	cout << cp1 << ", " << p2 << ", " << p3 << endl;
+	property<T> p7{p5};
+	property<Q> p8{std::move(p6)};
 
-	property<T> p10{"C++11"};
-	string s = "C++17";
-	property<T> p11(s);
-	property<Q> p12(std::move(s));
+	p5.add_update_event([](auto p) { cout << "~~~ p5 updated with value: " << p << endl; });
 
-	p11 = p10;
-	p11 = std::move(p10);
+	p5 = p7;
+	p5 = std::move(p8);
 
-	property<Q> p13{"C++20"};
-	p12 = p13;
-	p12 = std::move(p13);
+	((T&)p5).foo();
+	p5.invoke(&T::foo);*/
 
-
-
+/*
 	property<int*> pp1{new int}, pp2{new int};
 	property<const int*> pp3(std::move(pp1));
 	*pp2 = 42;
@@ -92,37 +55,13 @@ int main()
 	property<int*> x1;
 	x1 = std::move(pp2);
 
-	property<int[]> ap1{new int[3]}, ap2{new int[1]};
-	property<const int[]> ap3(std::move(ap1));
-	ap2[1] = 42;
-	cout << "property<int[]> = " << ap2[1] << " -> " << ap2 << endl;
-	cout << "property<int[]> = " << ap3[1] << " -> " << ap3 << endl;
-	property<int[]> x2;
-	x2 = std::move(ap2);
-
 	property<T*> pp4{new T}, pp5{new T};
 	property<const T*> pp6(std::move(pp4));
 	cout << "property<T*> = " << *pp5 << " -> " << pp5 << endl;
 	cout << "property<T*> = " << *pp6 << " -> " << pp6 << endl;
 
-	property<T[]> ap4{new T[3]}, ap5{new T[1]};
-	property<const T[]> ap6(std::move(ap4));
-	cout << "property<T[]> = " << ap5[1] << " -> " << ap5 << endl;
-	cout << "property<T[]> = " << ap6[1] << " -> " << ap6 << endl;
-
 	property<Q*> pp7{new Q};
 	property<T*> pp8(std::move(pp7));
 	cout << "property<Q*> = " << *pp8 << " -> " << pp8 << endl;
-
-	property<Q[]> ap7{new Q[1]};
-	property<T[]> ap8(std::move(ap7));
-	cout << "property<Q[]> = " << *ap8 << " -> " << ap8 << endl;
-
-
-
-	property<T> p14({1, 2, 3});
-	auto p15 = make_property<T>(4, 5, 6);
-
-	property<Q> p16({7, 8, 9});
-	auto p17 = make_property<Q>(10, 11, 12);
+*/
 }
--- src/property.hpp
+++ src/property.hpp
@@ -4,21 +4,33 @@
 #include <utility>
 #include <functional>
 #include <type_traits>
+#include <cstddef>
 
 template<typename T>
 class property
 {
 public:
+
+	template<typename U>
+	friend class property;
+
 	property() = default;
 
-	property(const T& v) : m_value(v) {}
-	property(T&& v) : m_value(std::move(v)) {}
+	explicit property(const T& v) : m_value(v) { std::cout << "property(const T& v)\n"; }
+	explicit property(T&& v) : m_value(std::move(v)) { std::cout << "property(T&& v)\n"; }
 
-	property(const property& p) : m_value(p.m_value) {}
-	property(property&& p) : m_value(std::move(p.m_value)) {}
+	property(const property& p) : m_value(p.m_value) { std::cout << "property(const property& p)\n"; }
+	property(property&& p) : m_value(std::move(p.m_value)) { std::cout << "property(property&& p)\n"; }
+
+	template<typename U>
+	property(const property<U>& p) : m_value(p.m_value) { std::cout << "property(const property<U>& p)\n"; }
+
+	template<typename U>
+	property(property<U>&& p) : m_value(std::move(p.m_value)) { std::cout << "property(property<U>&& p)\n"; }
 
 	property& operator = (const T& v)
 	{
+		std::cout << "operator = (const T& v)\n";
 		m_value = v;
 		fire_update_event();
 		return *this;
@@ -26,6 +38,7 @@ public:
 
 	property& operator = (T&& v)
 	{
+		std::cout << "operator = (T&& v)\n";
 		m_value = std::move(v);
 		fire_update_event();
 		return *this;
@@ -33,6 +46,7 @@ public:
 
 	property& operator = (const property& p)
 	{
+		std::cout << "operator = (const property& p)\n";
 		if(this != &p)
 		{
 			m_value = p.m_value;
@@ -43,6 +57,7 @@ public:
 
 	property& operator = (property&& p)
 	{
+		std::cout << "operator = (property&& p)\n";
 		if(this != &p)
 		{
 			m_value = std::move(p.m_value);
@@ -51,73 +66,80 @@ public:
 		return *this;
 	}
 
-	#ifndef PROPERTY_OPERATOR
-	#define PROPERTY_OPERATOR(op) \
-	property& operator op () \
+	template<typename U>
+	property& operator = (const property<U>& p)
+	{
+		std::cout << "operator = (const property<U>& p)\n";
+		m_value = p.m_value;
+		fire_update_event();
+		return *this;
+	}
+
+	template<typename U>
+	property& operator = (property<U>&& p)
+	{
+		std::cout << "operator = (property<U>&& p)\n";
+		m_value = std::move(p.m_value);
+		fire_update_event();
+		return *this;
+	}
+
+	#ifndef PROPERTY_INC_DEC_OPERATOR
+	#define PROPERTY_INC_DEC_OPERATOR(op) \
+	template<typename U = T> \
+	std::enable_if_t<std::is_arithmetic_v<U>, property&> \
+	operator op () \
 	{ \
 		op m_value; \
 		fire_update_event(); \
 		return *this; \
 	} \
-	property operator op (int) \
+	template<typename U = T> \
+	std::enable_if_t<std::is_arithmetic_v<U>, property> \
+	operator op (int) \
 	{ \
 		auto temp(*this); \
 		operator op (); \
 		return temp; \
 	}
-	PROPERTY_OPERATOR(++)
-	PROPERTY_OPERATOR(--)
-	#undef PROPERTY_OPERATOR
+	PROPERTY_INC_DEC_OPERATOR(++);
+	PROPERTY_INC_DEC_OPERATOR(--);
+	#undef PROPERTY_INC_DEC_OPERATOR
 	#endif
 
-	#ifndef PROPERTY_OPERATOR
-	#define PROPERTY_OPERATOR(op) \
-	template<typename Q> \
-	property& operator op (const Q& nv) \
+	#ifndef PROPERTY_ARITHMETIC_OPERATOR
+	#define PROPERTY_ARITHMETIC_OPERATOR(op) \
+	template<typename U = T> \
+	std::enable_if_t<std::is_arithmetic_v<U>, property&> \
+	operator op (const T& v) \
 	{ \
-		m_value op nv; \
+		m_value op v; \
 		fire_update_event(); \
 		return *this; \
 	}
-	PROPERTY_OPERATOR(+=)
-	PROPERTY_OPERATOR(-=)
-	PROPERTY_OPERATOR(*=)
-	PROPERTY_OPERATOR(/=)
-	PROPERTY_OPERATOR(%=)
-	PROPERTY_OPERATOR(^=)
-	PROPERTY_OPERATOR(&=)
-	PROPERTY_OPERATOR(|=)
-	PROPERTY_OPERATOR(>>=)
-	PROPERTY_OPERATOR(<<=)
-	#undef PROPERTY_OPERATOR
+	PROPERTY_ARITHMETIC_OPERATOR(+=);
+	PROPERTY_ARITHMETIC_OPERATOR(-=);
+	PROPERTY_ARITHMETIC_OPERATOR(*=);
+	PROPERTY_ARITHMETIC_OPERATOR(/=);
+	PROPERTY_ARITHMETIC_OPERATOR(%=);
+	PROPERTY_ARITHMETIC_OPERATOR(^=);
+	PROPERTY_ARITHMETIC_OPERATOR(&=);
+	PROPERTY_ARITHMETIC_OPERATOR(|=);
+	PROPERTY_ARITHMETIC_OPERATOR(>>=);
+	PROPERTY_ARITHMETIC_OPERATOR(<<=);
+	#undef PROPERTY_ARITHMETIC_OPERATOR
 	#endif
 
-	explicit operator T& () { return m_value; }
-	operator const T& () const { return m_value; }
+	explicit operator T& () { std::cout << "explicit operator T&\n"; return m_value; }
+	operator const T& () const { std::cout << "operator const T&\n"; return m_value; }
 
 	template<typename Q = T>
-	std::enable_if_t<std::is_pointer_v<Q> || std::is_class_v<Q>, T&>
+	std::enable_if_t<std::is_class_v<Q>, T&>
 	operator -> () { return m_value; }
 
 	template<typename Q = T>
-	std::enable_if_t<std::is_pointer_v<Q> || std::is_class_v<Q>, T&>
+	std::enable_if_t<std::is_class_v<Q>, const T&>
 	operator -> () const { return m_value; }
-
-	template<typename Q = T>
-	std::enable_if_t<std::is_pointer_v<Q>, std::remove_pointer_t<T>&>
-	operator * () { return *m_value; }
-
-	template<typename Q = T>
-	std::enable_if_t<std::is_pointer_v<Q>, const std::remove_pointer_t<T>&>
-	operator * () const { return *m_value; }
-
-	template<typename Q = T>
-	std::enable_if_t<std::is_pointer_v<Q>, std::remove_pointer_t<T>&>
-	operator [] (std::size_t i) { return m_value[i]; }
-
-	template<typename Q = T>
-	std::enable_if_t<std::is_pointer_v<Q>, const std::remove_pointer_t<T>&>
-	operator [] (std::size_t i) const { return m_value[i]; }
 
 	template<typename Q = T>
 	std::enable_if_t<std::is_class_v<Q>, typename Q::value_type&>
@@ -153,54 +175,121 @@ private:
 	}
 };
 
+
+
 template<typename T>
-class property<T[]>
+class property<T*>
 {
 public:
-	template<typename T2>
+	template<typename U>
 	friend class property;
 
-	property(T* v = nullptr) : m_value(v) {}
+	property() = default;
 
-	property(const property&) = delete;
+	explicit property(const T*& v) : m_value(v) {}
+	explicit property(T*&& v) : m_value(v) { v = nullptr; }
+
+	property(const property& p) : m_value(p.m_value) {}
 	property(property&& p) : m_value(p.m_value) { p.m_value = nullptr; }
 
-	template<typename T2>
-	property(const property<T2[]>&) = delete;
+	template<typename U>
+	property(const property<U*>& p) : m_value(p.m_value) {}
 
-	template<typename T2>
-	property(property<T2[]>&& p) : m_value(p.m_value) { p.m_value = nullptr; }
+	template<typename U>
+	property(property<U*>&& p) : m_value(p.m_value) { p.m_value = nullptr; }
 
-	property& operator = (const T*&) = delete;
+	property& operator = (const T*& v)
+	{
+		m_value = v;
+		fire_update_event();
+		return *this;
+	}
 
 	property& operator = (T*&& v)
 	{
-		delete [] m_value;
 		m_value = v;
 		v = nullptr;
 		fire_update_event();
 		return *this;
 	}
 
-	property& operator = (const property&) = delete;
-
-	property& operator = (property&& p)
+	property& operator = (const property& p)
 	{
-		delete [] m_value;
-		m_value = p.m_value;
-		p.m_value = nullptr;
-		fire_update_event();
+		if(this != &p)
+		{
+			m_value = p.m_value;
+			fire_update_event();
+		}
 		return *this;
 	}
 
-	T& operator [] (std::size_t i) const { return m_value[i]; }
+	property& operator = (property&& p)
+	{
+		if(this != &p)
+		{
+			m_value = p.m_value;
+			p.m_value = nullptr;
+			fire_update_event();
+		}
+		return *this;
+	}
 
+	#ifndef POINTER_INC_DEC_OPERATOR
+	#define POINTER_INC_DEC_OPERATOR(op) \
+	property& operator op () \
+	{ \
+		op m_value; \
+		fire_update_event(); \
+		return *this; \
+	} \
+	property operator op (int) \
+	{ \
+		auto temp(*this); \
+		operator op (); \
+		return temp; \
+	}
+	POINTER_INC_DEC_OPERATOR(++);
+	POINTER_INC_DEC_OPERATOR(--);
+	#undef POINTER_INC_DEC_OPERATOR
+	#endif
+
+	#ifndef POINTER_ARITHMETIC_OPERATOR
+	#define POINTER_ARITHMETIC_OPERATOR(op) \
+	property& operator op (std::ptrdiff_t v) \
+	{ \
+		m_value op v; \
+		fire_update_event(); \
+		return *this; \
+	}
+	POINTER_ARITHMETIC_OPERATOR(+=);
+	POINTER_ARITHMETIC_OPERATOR(-=);
+	#undef POINTER_ARITHMETIC_OPERATOR
+	#endif
+
+	explicit operator T* () { return m_value; }
 	operator const T* () const { return m_value; }
+
+	T* operator -> () { return m_value; }
+	const T* operator -> () const { return m_value; }
+
+	T& operator * () { return *m_value; }
+	const T& operator * () const { return *m_value; }
+
+	T& operator [] (std::size_t i) { return m_value[i]; }
+	const T& operator [] (std::size_t i) const { return m_value[i]; }
+
+	template<typename F, typename... Args>
+	auto invoke(F&& f, Args&&... args) -> std::invoke_result_t<F, T, Args...>
+	{
+		return std::invoke(std::forward<F>(f), m_value, std::forward<Args>(args)...);
+	}
 
 	using update_event_proc_t = std::function<void(const property&)>;
 
 	void add_update_event(update_event_proc_t&& proc) const
-	{ m_update_event_proc_list.push_back(std::forward<update_event_proc_t>(proc)); }
+	{
+		m_update_event_proc_list.push_back(std::forward<update_event_proc_t>(proc));
+	}
 
 private:
 	T* m_value = nullptr;
@@ -214,6 +303,8 @@ private:
 			proc(*this);
 	}
 };
+
+
 
 template<typename T, typename... Args>
 auto make_property(Args&&... args)
