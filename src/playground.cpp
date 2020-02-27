b391f41057840853d722889dc45e40cb612ca7bc
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2020-02-27 12:41:16 -0500

    decltype(auto) fun

--- src/playground.cpp
+++ src/playground.cpp
@@ -13,56 +13,15 @@
 
 using namespace std;
 
-void foo(const T& t) { cout << "foo(const T&) t = " << t << endl; }
-void foo(T&& t) { cout << "foo(T&&) t = " << t << endl; }
-template<typename T> void bar(T&& t) { foo(forward<T>(t)); }
-//void bar(T&& t) { foo(forward<T>(t)); } // Compile error, T&& is a rvalue reference here!
-
-template<typename X>
-struct S
+template<typename U>
+decltype(auto) foo(U&& v)
 {
-	using proc_t = function<void(const S&)>;
-
-	void foo(const proc_t&) { cout << "S::foo(const proc_t&)" << endl; }
-	void foo(proc_t&&) { cout << "S::foo(proc_t&&)" << endl; }
-
-	void bar(proc_t&& p) { S::foo(forward<proc_t>(p)); }
-};
+	return forward<U>(v);
+}
 
 int main()
 {
-	bar(T{"hi!"});
-	cout << endl;
-	T ttt{"bye!"};
-	bar(ttt);
-	cout << ">>> done" << endl << endl;
-
-	S<int> s;
-	s.bar([](auto&) {});
-	auto p = [](auto&) {};
-	s.bar(p);
-	cout << ">>> done" << endl << endl;
-
-	vector<T> vt;
-	vt.push_back(T{"hi!"});
-	vt.resize(2);
-	cout << ">>> done" << endl << endl;
-
-	vector<Q> vq;
-	vq.push_back(Q{"bye!"});
-	vq.resize(2);
-	cout << ">>> done" << endl << endl;
-
-	using XnXs = pair<int, int>;
-	using Xs = vector<XnXs>;
-	using XsPush = back_insert_iterator<Xs>;
-
-	int N = 10, S = 1;
-	Xs ints;
-
-	generate_n(XsPush(ints), N, [n = S]() mutable
-		{ auto p = make_pair(n, n * n); ++n; return p; });
-
-	for_each(begin(ints), end(ints), [](auto& p)
-		{ cout << p.first << ", " << p.second << endl; });
+	T t0{"C++"};
+	decltype(auto) t1 = (t0);
+	decltype(auto) t2 = foo(t1);
 }
