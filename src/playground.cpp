6776438db00fce2691e892645f8b0812a428ab51
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2020-03-06 15:05:35 -0500

    playing with bind and mem_fn

--- src/playground.cpp
+++ src/playground.cpp
@@ -12,16 +12,26 @@
 #include "T.hpp"
 
 using namespace std;
+using namespace std::placeholders;
 
-template<typename U>
-decltype(auto) foo(U&& v)
+template<typename T, typename... A>
+auto make_N_of_T(std::size_t N, A&&... a)
 {
-	return forward<U>(v);
+	std::vector<T> v;
+	v.reserve(N);
+	for(std::size_t i = 0; i < N; ++i)
+		v.emplace_back(std::forward<A>(a)...);
+	return v;
 }
 
 int main()
 {
-	T t0{"C++"};
-	decltype(auto) t1 = (t0);
-	decltype(auto) t2 = foo(t1);
+	cout << endl;
+	auto v = make_N_of_T<T>(5, "C++");
+
+	auto b3 = bind(&T::foo, _1);
+	for_each(begin(v), end(v), b3);
+
+	auto b4 = mem_fn(&T::bar);
+	for_each(begin(v), end(v), b4);
 }
