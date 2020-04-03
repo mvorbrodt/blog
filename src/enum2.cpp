7da5f51aa015fd7ead8022ce9a16323e51b21559
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2020-04-03 07:18:57 -0400

    Better enums

--- src/enum2.cpp
+++ src/enum2.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 
 #define MY_ENUM \
-X(V1) \
-X(V2) \
-X(V3)
+X(V1, -1) \
+X(V2, -3) \
+X(V3, -5)
 
-#define X(name) name,
+#define X(name, value) name = value,
 
-enum MyEnum
+#define MY_ENUM_NAME MyEnum
+#define MY_ENUM_TYPE int
+
+enum class MY_ENUM_NAME : MY_ENUM_TYPE
 {
 	MY_ENUM
 };
 
 #undef X
 
-constexpr const char* MyEnumToString(MyEnum e) noexcept
+constexpr auto MyEnumToString(MY_ENUM_NAME e) noexcept
 {
-#define X(name) case(name): return #name;
+#define X(name, value) case(MY_ENUM_NAME::name): return #name;
 	switch(e)
 	{
-			MY_ENUM
+		MY_ENUM
 	}
 #undef X
+	return "UNKNOWN";
 }
 
 int main(int argc, char** argv)
 {
-	std::cout << MyEnumToString(V1) << std::endl;
-	std::cout << MyEnumToString(V2) << std::endl;
-	std::cout << MyEnumToString(V3) << std::endl;
+	std::cout << "value = " << (MY_ENUM_TYPE)MyEnum::V1 << ", name = " << MyEnumToString(MY_ENUM_NAME::V1) << std::endl;
+	std::cout << "value = " << (MY_ENUM_TYPE)MyEnum::V2 << ", name = " << MyEnumToString(MY_ENUM_NAME::V2) << std::endl;
+	std::cout << "value = " << (MY_ENUM_TYPE)MyEnum::V3 << ", name = " << MyEnumToString(MY_ENUM_NAME::V3) << std::endl;
+
+	MY_ENUM_NAME unknown{-42};
+	std::cout << "value = " << (MY_ENUM_TYPE)unknown << ", name = " << MyEnumToString(unknown) << std::endl;
+
 	return 1;
 }
