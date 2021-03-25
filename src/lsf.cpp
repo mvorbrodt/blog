40eef5a17de42fa43783e3c3b186bed212e37641
mvorbrodt <martin.vorbrodt@icloud.com>
2021-03-25 11:14:58 -0400

    lsf improvements

--- src/lsf.cpp
+++ src/lsf.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+#include <arpa/inet.h>
 #include "lsf.hpp"
 
 // noisy test type, to see if we're not creating unnececary instances...
 struct N
 {
-	N(int v) : m_v{ v } { std::cout << "N(" << v << ")\n"; }
+	N(int v = -1) : m_v{ v } { std::cout << "N(" << v << ")\n"; }
 	N(const N& o) : m_v{ o.m_v } { std::cout << "N(const N&) " << m_v << "\n"; }
-	N(N&& o) : m_v{ o.m_v } { std::cout << "N(NN&) " << m_v << "\n"; }
+	N(N&& o) : m_v{ o.m_v } { std::cout << "N(N&&) " << m_v << "\n"; }
 	~N() { std::cout << "~N(" << m_v << ")\n"; }
-	N& operator = (const N& o) { std::cout << "operator=(const N&) " << m_v << ", " << o.m_v << "\n"; return *this; }
-	N& operator = (N&& o) { std::cout << "operator=(N&&) " << m_v << ", " << o.m_v << "\n"; return *this; }
+	N& operator = (N o) {
+		std::cout << "operator=(N) " << m_v << " = " << o.m_v << "\n";
+		std::swap(m_v, o.m_v); return *this; }
 
 private:
 	int m_v;
@@ -18,19 +23,103 @@ private:
 
 int main()
 {
-	using namespace std;
+	add_pack_transform<std::uint16_t>([](std::uint16_t v, buffer_output_t& it) { pack_value(it, htons(v)); });
+	add_pack_transform<std::uint32_t>([](std::uint32_t v, buffer_output_t& it) { pack_value(it, htonl(v)); });
+	add_pack_transform<std::uint64_t>([](std::uint64_t v, buffer_output_t& it) { pack_value(it, htonll(v)); });
 
-	auto buf_1 = pack(std::size_t(20));
-	auto tup_1 = unpack<std::size_t>(buf_1);
+	add_unpack_transform<std::uint16_t>([](buffer_input_t& it, std::uint16_t* v) { *v = ntohs(unpack_value<std::uint16_t>(it)); });
+	add_unpack_transform<std::uint32_t>([](buffer_input_t& it, std::uint32_t* v) { *v = ntohl(unpack_value<std::uint32_t>(it)); });
+	add_unpack_transform<std::uint64_t>([](buffer_input_t& it, std::uint64_t* v) { *v = ntohll(unpack_value<std::uint64_t>(it)); });
 
-	auto buf_2 = pack("this is a hardcoded string");
-	auto tup_2 = unpack<std::string>(buf_2);
+	auto buf_1 = pack(std::uint8_t(0x12), std::uint16_t(0x1234), std::uint32_t(0x12345678), std::uint64_t(0x1234567890ABCDEF));
+	auto tup_1 = unpack<std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t>(buf_1);
 
-	auto buf_3 = pack("this is std::string"s);
-	auto tup_3 = unpack<std::string>(buf_3);
 
-	auto buf_4 = pack('C', '+', '+', 11, 14, 17, "this is a hardcoded string", "this is std::string"s, N(20), N(23));
-	auto tup_4 = unpack<char, char, char, int, int, int, std::string, std::string, N, N>(buf_4);
+
+	add_pack_transform<std::size_t>([](std::size_t v, buffer_output_t& it) { pack_value(it, std::uint8_t(v)); });
+	add_unpack_transform<std::size_t>([](buffer_input_t& it, std::size_t* v) { *v = unpack_value<std::uint8_t>(it); });
+	add_pack_size_proc<std::size_t>([](std::size_t) { return sizeof(std::uint8_t); });
+
+	auto buf_2_1 = pack(std::size_t(-1));
+	auto tup_2_1 = unpack<std::size_t>(buf_2_1);
+
+	remove_pack_transform<std::size_t>();
+	remove_unpack_transform<std::size_t>();
+	remove_pack_size_proc<std::size_t>();
+
+	auto buf_2_2 = pack(std::size_t(-1));
+	auto tup_2_2 = unpack<std::size_t>(buf_2_2);
+
+
+
+	add_pack_transform<const char*>(
+		[](const char* v, buffer_output_t& it)
+		{
+			auto len = std::strlen(v);
+			pack_type(it, len);
+			pack_bytes(it, v, len);
+		});
+
+	add_pack_transform<std::string>(
+		[](const std::string& v, buffer_output_t& it)
+		{
+			pack_type(it, v.length());
+			pack_bytes(it, v.data(), v.length());
+		});
+
+	add_unpack_transform<std::string>(
+		[](buffer_input_t& it, std::string* v)
+		{
+			auto len = unpack_type<std::string::size_type>(it);
+			new (v) std::string(len, std::string::value_type()); // do this cleanly !!!
+			unpack_bytes(it, v->data(), len);
+		});
+
+	add_pack_size_proc<const char*>([](const char* v) { return pack_size(std::strlen(v)) + std::strlen(v); });
+	add_pack_size_proc<std::string>([](const std::string& v) { return pack_size(v.length()) + v.length(); });
+
+	auto ccp_1 = "this is hardcoded string #1";
+	auto buf_3 = pack(ccp_1, "this is hardcoded string #2");
+	auto tup_3 = unpack<std::string, std::string>(buf_3);
+
+	auto str_1 = std::string("this is std::string #1");
+	auto buf_4 = pack(str_1, std::string("this is std::string #2"));
+	auto tup_4 = unpack<std::string, std::string>(buf_4);
+
+
+
+	auto n_1 = N(11);
+	auto buf_5 = pack(n_1, N(17));
+	auto tup_5 = unpack<N, N>(buf_5);
+
+	auto buf_6 = pack('C', '+', '+', 11, 14, 17, "this is hardcoded string", std::string("this is std::string"), N(20), N(23));
+	auto tup_6 = unpack<char, char, char, int, int, int, std::string, std::string, N, N>(buf_6);
+
+
+
+	using strings_t = std::vector<std::string>;
+
+	add_pack_transform<strings_t>(
+		[](const strings_t& vs, buffer_output_t& it)
+		{
+			pack_type(it, vs.size());
+			std::for_each(std::begin(vs), std::end(vs),
+				[&](const std::string& s) { pack_type(it, s); });
+		});
+
+	add_unpack_transform<strings_t>(
+		[](buffer_input_t& it, strings_t* vs)
+		{
+			auto size = unpack_type<strings_t::size_type>(it);
+			new (vs) strings_t();
+			vs->reserve(size);
+			std::generate_n(std::back_inserter(*vs), size,
+				[&]() { return unpack_type<std::string>(it); });
+		});
+
+	auto vec_1 = strings_t{ "string 1", "string 2", "string 3" };
+	auto buf_7 = pack(vec_1);
+	auto tup_7 = unpack<strings_t>(buf_7);
 
 	return rand();
 }
--- src/lsf.hpp
+++ src/lsf.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <tuple>
-#include <string>
 #include <vector>
 #include <utility>
 #include <iterator>
@@ -13,64 +12,176 @@
 
 
 
-template<typename T>
-constexpr decltype(auto) pack_type_transform(T&& value) { return(value); }
-
-template<typename T>
-constexpr decltype(auto) unpack_type_transform(T&& value) { return(value); }
-
-#define MAKE_PACK_TYPE_CAST(from, to) \
-	inline auto pack_type_transform(from value) { return to(value); } \
-	inline auto unpack_type_transform(to value) { return from(value); }
-
-#define MAKE_PACK_TYPE_TRANSFORM(from, to, pack_proc, unpack_proc) \
-	inline decltype(auto) pack_type_transform(from&& value) { return(pack_proc(std::forward<from>(value))); } \
-	inline decltype(auto) unpack_type_transform(to&& value) { return(unpack_proc(std::forward<to>(value))); }
-
-#define PACK_TYPE_FROM_TYPE(T) std::decay_t<decltype(pack_type_transform(std::declval<T>()))>
-
-MAKE_PACK_TYPE_CAST(std::size_t, short);
-
-MAKE_PACK_TYPE_TRANSFORM(const char*, std::string,
-	[](auto v) { return std::string(v); },
-	[](auto v) { return v; });
-
-
-
-#define PACKED_TYPE_SIZE(T) sizeof(PACK_TYPE_FROM_TYPE(T))
-
-template<typename T>
-auto packed_type_size(const T&) { return PACKED_TYPE_SIZE(T); }
-
-#define MAKE_PACK_TYPE_SIZE(type, proc) \
-	inline auto packed_type_size(const std::decay_t<type>& value) { return proc(value); }
-
-MAKE_PACK_TYPE_SIZE(const char*, [](auto v) { return sizeof(std::strlen(v)) + std::strlen(v); });
-MAKE_PACK_TYPE_SIZE(std::string, [](auto v) { return sizeof(v.length()) + v.length(); });
-
-
-
-using buffer_t = std::vector<std::byte>;
+using byte_t = std::byte;
+using buffer_t = std::vector<byte_t>;
 using buffer_ptr_t = buffer_t::pointer;
 using buffer_input_t = buffer_t::const_iterator;
 using buffer_output_t = std::back_insert_iterator<buffer_t>;
 
-struct buffer_walker_t
+struct on_exit_t
 {
-	buffer_walker_t(buffer_input_t& it, std::ptrdiff_t dist) : m_it{ it }, m_dist{ dist } {}
-	~buffer_walker_t() { m_it += m_dist; }
-
-private:
-	buffer_input_t& m_it;
-	std::ptrdiff_t m_dist;
+	using proc_t = std::function<void()>;
+	on_exit_t(proc_t&& proc) : m_proc{ std::move(proc) } {}
+	~on_exit_t() { m_proc(); }
+	proc_t m_proc;
 };
 
 
 
-template<typename T>
-void pack_bytes(buffer_output_t& it, const T* ptr, std::size_t count)
+inline void pack_bytes(buffer_output_t& it, const void* ptr, std::size_t count);
+inline void unpack_bytes(buffer_input_t& it, void* ptr, std::size_t count);
+
+template<typename T> void pack_value(buffer_output_t& it, const T& value);
+template<typename T> decltype(auto) unpack_value(buffer_input_t& it);
+
+
+
+using type_hash_t = decltype(std::declval<std::type_info>().hash_code());
+
+using pack_transform_t = std::function<void(const void*, buffer_output_t&)>;
+using unpack_transform_t = std::function<void(buffer_input_t&, void*)>;
+
+using pack_transform_map_t = std::unordered_map<type_hash_t, pack_transform_t>;
+using unpack_transform_map_t = std::unordered_map<type_hash_t, unpack_transform_t>;
+
+using transform_maps_t = std::pair<pack_transform_map_t, unpack_transform_map_t>;
+
+inline decltype(auto) get_transform_maps()
 {
-	std::copy(buffer_ptr_t(ptr), buffer_ptr_t(ptr) + count, it);
+	static auto k_transform_maps = transform_maps_t();
+
+	return(k_transform_maps);
+}
+
+
+
+template<typename T, typename P>
+auto add_pack_transform(P proc)
+{
+	auto& [pack_map, _] = get_transform_maps();
+	auto hash = typeid(T).hash_code();
+	auto transform = pack_transform_t([=](const void* pv, buffer_output_t& it) { proc(*((const T*)pv), it); });
+
+	auto [it, result] = pack_map.insert_or_assign(hash, transform);
+	return result == true;
+}
+
+template<typename T>
+decltype(auto) get_pack_transform()
+{
+	using DT = std::decay_t<T>;
+	static auto k_default_pack_transform = pack_transform_t([](const void* p, buffer_output_t& it) { pack_value(it, *((const DT*)p)); });
+
+	auto& [pack_map, _] = get_transform_maps();
+	auto hash = typeid(DT).hash_code();
+	auto it = pack_map.find(hash);
+	if(it != std::end(pack_map))
+		return((*it).second);
+
+	return(k_default_pack_transform);
+}
+
+template<typename T>
+auto remove_pack_transform()
+{
+	auto& [pack_map, _] = get_transform_maps();
+	auto hash = typeid(T).hash_code();
+	auto result = pack_map.erase(hash);
+
+	return result > 0;
+}
+
+
+
+template<typename T, typename P>
+auto add_unpack_transform(P proc)
+{
+	auto& [_, unpack_map] = get_transform_maps();
+	auto hash = typeid(T).hash_code();
+	auto transform = unpack_transform_t([=](buffer_input_t& it, void* pv) { proc(it, (T*)pv); });
+
+	auto [it, result] = unpack_map.insert_or_assign(hash, transform);
+	return result == true;
+}
+
+template<typename T>
+decltype(auto) get_unpack_transform()
+{
+	static auto k_default_unpack_transform = unpack_transform_t([](buffer_input_t& it, void* p) { new (p) T(unpack_value<T>(it)); });
+
+	auto& [_, unpack_map] = get_transform_maps();
+	auto hash = typeid(T).hash_code();
+	auto it = unpack_map.find(hash);
+	if(it != std::end(unpack_map))
+		return((*it).second);
+
+	return(k_default_unpack_transform);
+}
+
+template<typename T>
+auto remove_unpack_transform()
+{
+	auto& [_, unpack_map] = get_transform_maps();
+	auto hash = typeid(T).hash_code();
+	auto result = unpack_map.erase(hash);
+
+	return result > 0;
+}
+
+
+
+using pack_size_proc_t = std::function<std::size_t(const void*)>;
+using pack_size_proc_map_t = std::unordered_map<type_hash_t, pack_size_proc_t>;
+
+inline decltype(auto) get_pack_size_proc_map()
+{
+	static auto k_pack_size_proc_map = pack_size_proc_map_t();
+
+	return(k_pack_size_proc_map);
+}
+
+
+
+template<typename T, typename P>
+auto add_pack_size_proc(P proc)
+{
+	decltype(auto) proc_map = get_pack_size_proc_map();
+	auto hash = typeid(T).hash_code();
+	auto size_proc = pack_size_proc_t([=](const void* pv) { return proc(*((const T*)pv)); });
+
+	auto [it, result] = proc_map.insert_or_assign(hash, size_proc);
+	return result == true;
+}
+
+template<typename T>
+decltype(auto) get_pack_size_proc()
+{
+	static auto k_default_pack_size_proc = pack_size_proc_t([](const void*) { return sizeof(T); });
+
+	decltype(auto) proc_map = get_pack_size_proc_map();
+	auto hash = typeid(T).hash_code();
+	auto it = proc_map.find(hash);
+	if(it != std::end(proc_map))
+		return((*it).second);
+
+	return(k_default_pack_size_proc);
+}
+
+template<typename T>
+auto remove_pack_size_proc()
+{
+	decltype(auto) proc_map = get_pack_size_proc_map();
+	auto hash = typeid(T).hash_code();
+	auto result = proc_map.erase(hash);
+
+	return result > 0;
+}
+
+
+
+inline void pack_bytes(buffer_output_t& it, const void* ptr, std::size_t count)
+{
+	std::copy_n(buffer_ptr_t(ptr), count, it);
 }
 
 template<typename T>
@@ -80,80 +191,85 @@ void pack_value(buffer_output_t& it, const T& value)
 }
 
 template<typename T>
-void pack_type(buffer_output_t& it, const T& value)
+void pack_type(buffer_output_t& it, T&& value)
 {
-	pack_value(it, value);
-}
-
-inline void pack_type(buffer_output_t& it, const std::string& value)
-{
-	pack_type(it, value.length());
-	pack_bytes(it, value.data(), value.length());
+	decltype(auto) transform = get_pack_transform<T>();
+	transform(&value, it);
 }
 
 template<typename T>
-void pack_it(buffer_output_t& it, T&& value)
+void pack_type(buffer_output_t& it, const T* ptr)
 {
-	pack_type(it, pack_type_transform(std::forward<T>(value)));
+	decltype(auto) transform = get_pack_transform<const T*>();
+	transform(&ptr, it);
 }
 
-template<typename... T>
-auto packed_size(T&&... value)
+template<typename T>
+auto pack_size(T&& value)
 {
-	auto buffer_size = ((packed_type_size(std::forward<T>(value))) + ...);
-	return buffer_size;
+	auto size_proc = get_pack_size_proc<T>();
+	auto type_size = size_proc(&value);
+	return type_size;
+}
+
+template<typename T>
+auto pack_size(const T* ptr)
+{
+	auto size_proc = get_pack_size_proc<const T*>();
+	auto type_size = size_proc(&ptr);
+	return type_size;
 }
 
 template<typename... T>
 auto pack(T&&... value)
 {
 	auto buffer = buffer_t();
+	auto buffer_size = ((pack_size(std::forward<T>(value)))+...);
+	buffer.reserve(buffer_size);
+
 	auto it = buffer_output_t(buffer);
-	buffer.reserve(packed_size(std::forward<T>(value)...));
-	(pack_it(it, std::forward<T>(value)),...);
+	(pack_type(it, std::forward<T>(value)),...);
+
 	return buffer;
 }
 
 
 
 template<typename T>
-decltype(auto) value_cast(buffer_input_t& it)
+decltype(auto) value_cast(const void* ptr)
 {
 	using value_ptr_t = const T*;
-	return(*value_ptr_t(&*it));
+	return(*value_ptr_t(ptr));
+}
+
+inline void unpack_bytes(buffer_input_t& it, void* ptr, std::size_t count)
+{
+	auto walk_it = on_exit_t([&]() { it += count; });
+	std::copy_n(it, count, buffer_ptr_t(ptr));
 }
 
 template<typename T>
 decltype(auto) unpack_value(buffer_input_t& it)
 {
-	auto walk = buffer_walker_t(it, sizeof(T));
-	return(value_cast<T>(it));
+	auto walk_it = on_exit_t([&]() { it += sizeof(T); });
+	return(value_cast<T>(&*it));
 }
 
 template<typename T>
-decltype(auto) unpack_type(buffer_input_t& it)
+auto unpack_type(buffer_input_t& it)
 {
-	return(unpack_value<T>(it));
-}
+	byte_t storage[sizeof(T)] = {};
+	auto clean_it = on_exit_t([&]() { value_cast<T>(storage).~T(); });
 
-template<>
-decltype(auto) unpack_type<std::string>(buffer_input_t& it)
-{
-	auto length = unpack_value<std::string::size_type>(it);
-	auto walk = buffer_walker_t(it, length);
-	return(std::string(std::string::const_pointer(&*it), length));
-}
+	decltype(auto) transform = get_unpack_transform<T>();
+	transform(it, storage);
 
-template<typename T>
-decltype(auto) unpack_it(buffer_input_t& it)
-{
-	using PT = PACK_TYPE_FROM_TYPE(T);
-	return(unpack_type_transform(unpack_type<PT>(it)));
+	return value_cast<T>(storage);
 }
 
 template<typename... T>
 auto unpack(const buffer_t& buffer)
 {
 	auto it = std::begin(buffer);
-	return std::tuple<T...>((unpack_it<T>(it))...);
+	return std::tuple<T...>((unpack_type<T>(it))...);
 }
