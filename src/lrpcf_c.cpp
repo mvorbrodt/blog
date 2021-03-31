c6b617578b7987f3964f2f059824b225ddae2224
mvorbrodt <martin.vorbrodt@icloud.com>
2021-03-31 18:14:24 -0400

    wait on event before allowing more input

--- src/lrpcf_c.cpp
+++ src/lrpcf_c.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include "event.hpp"
 #include "socket.hpp"
 
 int main(int argc, char** argv)
@@ -18,10 +19,12 @@ int main(int argc, char** argv)
 		auto host = argv[1];
 		auto port = stoul(argv[2]);
 		auto client = client_socket(host, port);
+		auto event = auto_event(false);
 
-		client.set_data_handler([](client_socket& cs, socket_buffer_t data)
+		client.set_data_handler([&](client_socket& cs, socket_buffer_t data)
 		{
 			cout << "< " << string((const char*)data.data(), data.size()) << endl;
+			event.signal();
 		});
 
 		thread([&]()
@@ -37,7 +40,7 @@ int main(int argc, char** argv)
 					continue;
 
 				client.send(line.data(), line.length());
-				this_thread::sleep_for(250ms);
+				event.wait();
 			}
 		}).detach();
 
