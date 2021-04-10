0e55c55c69e57c4ce57ea2a8b2d898c262c97388
mvorbrodt <martin.vorbrodt@icloud.com>
2021-04-10 15:37:41 -0400

    possibly final istring code

--- src/istring.cpp
+++ src/istring.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
-#include "istrcmp.hpp"
+#include <iomanip>
+#include "istring.hpp"
+using namespace std;
 
-void f_std(const std::string& s) { std::cout << s << std::endl; }
-
-void f_is(const istring& s) { std::cout << s << std::endl; }
+void f_std(const string& s) {}
+void f_is(const istring& s) {}
 
 int main()
 {
-	using namespace std;
-
 	string std1{"abc"};
 	istring istr1{"ABC"};
-	auto result = (std1 == istr1);
 
 	f_std(istr1);
 	f_is(std1);
 
-	auto s1 = "abc"s;
-	auto s2 = "ABC"s;
-	cout << "'" << s1 << "' == '" << s2 << "' str_icmp: " << str_icmp(s1, s2) << endl;
-
-	s1 = "aaa"s;
-	s2 = "bbb"s;
-	cout << "'" << s1 << "' == '" << s2 << "' str_icmp: " << str_icmp(s1, s2) << endl;
-
-	s1 = "bbb"s;
-	s2 = "aaa"s;
-	cout << "'" << s1 << "' == '" << s2 << "' str_icmp: " << str_icmp(s1, s2) << endl << endl;
+	auto result = bool{};
+	result = (std1 == istr1);
+	result = (std1 != istr1);
+	result = (std1 <  istr1);
+	result = (std1 <= istr1);
+	result = (std1 > istr1);
+	result = (std1 >= istr1);
 
 	auto is1 = istring{ "aaa" };
 	auto is2 = istring{ "AAA" };
@@ -41,7 +35,7 @@ int main()
 	is1 = "bbb"_is;
 	is2 = "aaa"_is;
 	cout << "'" << is1 << "' == '" << is2 << "' 2-way: " << boolalpha
-		<< (is1 == is2) << ", 3-way: " << is1.compare(is2) << endl << endl;
+		<< (is1 == is2) << ", 3-way: " << is1.compare(is2) << endl;
 
 	auto iws1 = iwstring{ L"aaa" };
 	auto iws2 = iwstring{ L"AAA" };
--- src/istring.hpp
+++ src/istring.hpp
@@ -2,79 +2,54 @@
 
 #include <istream>
 #include <ostream>
-#include <iomanip>
 #include <compare>
 #include <string>
 #include <locale>
-#include <memory>
 #include <utility>
 #include <algorithm>
-#include <functional>
 #include <type_traits>
-
-
 
 inline namespace detail
 {
 	template<typename CharT>
-	inline auto char_ieq(CharT c1, CharT c2, const std::locale& loc)
+	inline auto char_ieq(CharT c1, CharT c2, const std::locale& loc = std::locale())
 	{
 		return std::toupper(c1, loc) == std::toupper(c2, loc);
 	};
 
 	template<typename CharT>
-	inline auto char_ilt(CharT c1, CharT c2, const std::locale& loc)
+	inline auto char_ilt(CharT c1, CharT c2, const std::locale& loc = std::locale())
 	{
 		return std::toupper(c1, loc) < std::toupper(c2, loc);
 	};
+
+	template<typename CharT>
+	inline auto string_icmp(const CharT* s1, std::size_t n1, const CharT* s2, std::size_t n2, const std::locale& loc = std::locale())
+	{
+		if(std::lexicographical_compare(s1, s1 + n1, s2, s2 + n2, [&](CharT c1, CharT c2) { return char_ilt(c1, c2, loc); })) return -1;
+		if(std::lexicographical_compare(s2, s2 + n2, s1, s1 + n1, [&](CharT c1, CharT c2) { return char_ilt(c1, c2, loc); })) return 1;
+		return 0;
+	}
+
+	template<typename CharT>
+	struct char_itraits : std::char_traits<CharT>
+	{
+		static auto eq(CharT c1, CharT c2)
+		{
+			return char_ieq(c1, c2);
+		}
+
+		static auto lt(CharT c1, CharT c2)
+		{
+			return char_ilt(c1, c2);
+		}
+
+		static auto compare(const CharT* s1, const CharT* s2, std::size_t n)
+		{
+			return string_icmp(s1, n, s2, n);
+		}
+	};
 }
-
-
-
-/*template<typename CharT, typename T1, typename A1, typename T2, typename A2>
-inline auto str_ieq(const std::basic_string<CharT, T1, A1>& s1, const std::basic_string<CharT, T2, A2>& s2, const std::locale& loc = std::locale())
-{
-	return std::equal(std::begin(s1), std::end(s1), std::begin(s2), std::end(s2),
-		[&](CharT c1, CharT c2) { return char_ieq(c1, c2, loc); });
-}*/
-
-template<typename CharT, typename T1, typename A1, typename T2, typename A2>
-inline auto str_icmp(const std::basic_string<CharT, T1, A1>& s1, const std::basic_string<CharT, T2, A2>& s2, const std::locale& loc = std::locale())
-{
-	auto r = std::lexicographical_compare(std::begin(s1), std::end(s1), std::begin(s2), std::end(s2),
-		[&](CharT c1, CharT c2) { return char_ilt(c1, c2, loc); });
-	if(r) return -1;
-	r = std::lexicographical_compare(std::begin(s2), std::end(s2), std::begin(s1), std::end(s1),
-		[&](CharT c1, CharT c2) { return char_ilt(c1, c2, loc); });
-	if(r) return 1;
-	return 0;
-}
-
-
-
-// case insensitive version of std::char_traits...
-// ...use with std::basic_string to create case insensitive string class
-template<typename CharT>
-struct char_itraits : std::char_traits<CharT>
-{
-	static auto eq(CharT c1, CharT c2)
-	{
-		return char_ieq(c1, c2);
-	}
-
-	static auto lt(CharT c1, CharT c2)
-	{
-		return char_ilt(c1, c2);
-	}
-
-	static auto compare(const CharT* s1, const CharT* s2, std::size_t n)
-	{
-		static const auto loc = std::locale();
-		return str_icmp(std::basic_string<CharT>{s1, n}, std::basic_string<CharT>{s2, n}, loc);
-	}
-};
-
-
 
 template<typename CharT, typename Alloc = std::allocator<CharT>>
 class basic_istring : public std::basic_string<CharT, char_itraits<CharT>, Alloc>
@@ -97,47 +72,33 @@ public:
 	std::enable_if_t<not std::is_same_v<char_itraits<CharT>, Traits2>, bool>
 	friend operator == (const basic_istring& lhs, std::basic_string<CharT, Traits2, Alloc2>& rhs)
 	{
-		return str_icmp(lhs, rhs) == 0;
+		return string_icmp(lhs.data(), lhs.length(), rhs.data(), rhs.length()) == 0;
 	}
 
 	template<typename Traits2, typename Alloc2>
 	std::enable_if_t<not std::is_same_v<char_itraits<CharT>, Traits2>, std::strong_ordering>
 	friend operator <=> (const basic_istring& lhs, std::basic_string<CharT, Traits2, Alloc2>& rhs)
 	{
-		return str_icmp(lhs, rhs) <=> 0;
+		return string_icmp(lhs.data(), lhs.length(), rhs.data(), rhs.length()) <=> 0;
 	}
 
 	template<typename Traits2, typename Alloc2>
 	std::enable_if_t<not std::is_same_v<char_itraits<CharT>, Traits2>, bool>
 	friend operator == (std::basic_string<CharT, Traits2, Alloc2>& lhs, const basic_istring& rhs)
 	{
-		return str_icmp(lhs, rhs) == 0;
+		return string_icmp(lhs.data(), lhs.length(), rhs.data(), rhs.length()) == 0;
 	}
 
 	template<typename Traits2, typename Alloc2>
 	std::enable_if_t<not std::is_same_v<char_itraits<CharT>, Traits2>, std::strong_ordering>
 	friend operator <=> (std::basic_string<CharT, Traits2, Alloc2>& lhs, const basic_istring& rhs)
 	{
-		return str_icmp(lhs, rhs) <=> 0;
+		return string_icmp(lhs.data(), lhs.length(), rhs.data(), rhs.length()) <=> 0;
 	}
 };
 
-
-
 using istring = basic_istring<char>;
 using iwstring = basic_istring<wchar_t>;
-
-
-
-inline auto operator ""_is(const char* istr, std::size_t len)
-{
-	return istring(istr, len);
-}
-
-inline auto operator ""_iws(const wchar_t* iwstr, std::size_t len)
-{
-	return iwstring(iwstr, len);
-}
 
 inline auto& operator >> (std::istream& is, istring& istr)
 {
@@ -165,4 +126,14 @@ inline auto& operator << (std::wostream& wos, const iwstring& iwstr)
 {
 	wos << iwstr.c_str();
 	return wos;
+}
+
+inline auto operator ""_is(const char* istr, std::size_t len)
+{
+	return istring(istr, len);
+}
+
+inline auto operator ""_iws(const wchar_t* iwstr, std::size_t len)
+{
+	return iwstring(iwstr, len);
 }
