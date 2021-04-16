afd0144424b52e91de6d601091f231bfad1afe2d
mvorbrodt <martin.vorbrodt@icloud.com>
2021-04-16 13:38:39 -0400

    tweak

--- run_cmake_msvc.bat
+++ run_cmake_msvc.bat
@@ -1,6 +1,6 @@
 @echo off
-copy /Y CMakeLists.win.txt CMakeLists.txt
-copy /Y src\CMakeLists.win.txt src\CMakeLists.txt
+copy /Y CMakeLists.txt.msvc CMakeLists.txt
+copy /Y src\CMakeLists.txt.msvc src\CMakeLists.txt
 mkdir msvc
 cd msvc
 cmake -DCMAKE_TOOLCHAIN_FILE=C:\Code\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows ..
