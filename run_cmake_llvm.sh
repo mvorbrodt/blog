224c4b059cbef199e1f27ff73a28f0fb13e6f329
mvorbrodt <martin.vorbrodt@icloud.com>
2021-04-16 23:18:26 -0400

    yey latest llvm

--- run_cmake_llvm.sh
+++ run_cmake_llvm.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # Old paths using brew's llvm
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-export CC=/usr/local/opt/llvm/bin/clang
-export CXX=/usr/local/opt/llvm/bin/clang++
-export LD=/usr/local/opt/llvm/bin/ld.lld
-export AR=/usr/local/opt/llvm/bin/llvm-ar
-export RANLIB=/usr/local/opt/llvm/bin/llvm-ranlib
-export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/"
-export CXXFLAGS="$CPPFLAGS"
-
-# export PATH="/usr/local/bin:$PATH"
-# export CC=/usr/local/bin/clang
-# export CXX=/usr/local/bin/clang++
-# export LD=/usr/local/bin/ld.lld
-# export AR=/usr/local/bin/llvm-ar
-# export RANLIB=/usr/local/bin/llvm-ranlib
-# export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-# export CPPFLAGS="-I/usr/local/include -I/usr/local/include/c++/v1/"
+# export PATH="/usr/local/opt/llvm/bin:$PATH"
+# export CC=/usr/local/opt/llvm/bin/clang
+# export CXX=/usr/local/opt/llvm/bin/clang++
+# export LD=/usr/local/opt/llvm/bin/ld.lld
+# export AR=/usr/local/opt/llvm/bin/llvm-ar
+# export RANLIB=/usr/local/opt/llvm/bin/llvm-ranlib
+# export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
+# export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/"
 # export CXXFLAGS="$CPPFLAGS"
+
+export PATH="/usr/local/bin:$PATH"
+export CC=/usr/local/bin/clang
+export CXX=/usr/local/bin/clang++
+export LD=/usr/local/bin/ld.lld
+export AR=/usr/local/bin/llvm-ar
+export RANLIB=/usr/local/bin/llvm-ranlib
+export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
+export CPPFLAGS="-I/usr/local/include -I/usr/local/include/c++/v1/"
+export CXXFLAGS="$CPPFLAGS"
 
 alias cc=$CC
 alias c++=$CXX
