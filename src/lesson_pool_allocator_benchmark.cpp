498ee90c28c3bca64ad1a55f5009546fa05383a7
mvorbrodt <martin.vorbrodt@icloud.com>
2021-05-26 21:49:05 -0400

    add lesson samples to cmake

--- src/lesson_pool_allocator_benchmark.cpp
+++ src/lesson_pool_allocator_benchmark.cpp
@@ -5,16 +5,33 @@
 #include <vector>
 #include <thread>
 #include <string>
+#include <random>
+#include <algorithm>
 #include <boost/pool/singleton_pool.hpp>
+#include <cstdint>
+#include <cstring>
 
 using namespace std;
 
-using payload_t = unsigned long long;
-auto payload = payload_t{};
+using seq_t = uint64_t;
+using payload_t = struct { std::byte data[16]; };
+
+static_assert(sizeof(payload_t) >= sizeof(seq_t));
+
+auto gen_next_payload()
+{
+	static auto seq = seq_t{ 1 };
+
+	payload_t p;
+	memcpy(&p, &seq, sizeof(seq));
+	++seq;
+
+	return p;
+}
 
 struct P
 {
-	P() : m_v{ ++payload } {}
+	P() : m_v{ gen_next_payload() } {}
 	payload_t m_v;
 
 	using pool = boost::singleton_pool<P, sizeof(payload_t)>;
@@ -30,7 +47,7 @@ static_assert(sizeof(P) == sizeof(payload_t));
 
 struct NP
 {
-	NP() : m_v{ ++payload } {}
+	NP() : m_v{ gen_next_payload() } {}
 	payload_t m_v;
 };
 
@@ -39,12 +56,18 @@ static_assert(sizeof(NP) == sizeof(payload_t));
 template<typename T>
 void run_impl(int c, int n)
 {
+	static mt19937 gen((random_device{})());
+
 	vector<T*> v;
 	v.reserve(n);
 	for(auto i = 0; i < c; ++i)
 	{
 		for(auto it = 0; it < n; ++it)
 			v.push_back(new T);
+
+		// simulate randomness in allocations/deallocations
+		shuffle(begin(v), end(v), gen);
+
 		for(auto p : v)
 			delete p;
 		v.clear();
@@ -61,16 +84,17 @@ template<typename T>
 void runN(int c, int n, int ts)
 {
 	vector<thread> v;
+	v.reserve(ts);
 	for(auto t = 0; t < ts; ++t)
 		v.emplace_back([=]() { run_impl<T>(c, n); });
 	for(auto& t : v)
 		t.join();
 }
 
-TEST_CASE("Pool Allocator", "[benchmark]")
+TEST_CASE("Pool Allocator. Chunk size = "s + to_string(sizeof(payload_t)), "[benchmark]")
 {
-	auto C = 1'000;
-	auto N = 100;
+	auto C = 10;
+	auto N = 100'000;
 	auto T = thread::hardware_concurrency();
 
 	BENCHMARK("1 thread w/ pool")
