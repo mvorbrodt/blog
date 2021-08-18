2a5a5c17920e3592e6a9caa572b14575ac9ca62f
mvorbrodt <martin.vorbrodt@icloud.com>
2021-08-18 14:00:02 -0400

    shared_synchronized with support for reader/writer locks

--- src/synchronized.cpp
+++ src/synchronized.cpp
@@ -1,29 +1,63 @@
+#include <iostream>
 #include <thread>
 #include <vector>
 #include <cstdlib>
 #include "synchronized.hpp"
 
+struct S
+{
+	void method() { std::cout << "non const\n"; }
+	void method() const { std::cout << "const\n"; }
+};
+
 int main()
 {
-	using namespace std;
+	shared_synchronized<S> s1;
 
-	srand(time(NULL));
+	// takes exclusive lock
+	s1->method();
 
-	synchronized<vector<int>> sv;
-	//auto sv = new vector<int>;
+	// takes shared lock
+	std::as_const(s1)->method();
 
-	thread t1([&] ()
+	// due to const'ness calling any method on s2, s3, or s4
+	// will ALWAYS take shared lock; no need for std::as_const
+	const shared_synchronized<S> s2;
+	shared_synchronized<const S> s3;
+	const shared_synchronized<const S> s4;
+
+	// all 3 calls take shared lock
+	s2->method();
+	s3->method();
+	s4->method();
+
+	// std::vector shared by 3 threads t1, t2, and t3
+	// t1 and t2 take exclusive locks, t3 takes shared lock
+	shared_synchronized<std::vector<int>> sv;
+
+	std::thread t1([&] ()
 	{
+		std::srand(std::time(NULL));
 		while(true)
+			// takes exclusive lock
 			sv->push_back(rand());
 	});
 
-	thread t2([&] ()
+	std::thread t2([&] ()
 	{
 		while(true)
+			// takes exclusive lock
 			sv->clear();
+	});
+
+	std::thread t3([&] ()
+	{
+		while(true)
+			// takes shared lock
+			[[maybe_unused]] auto _ = std::as_const(sv)->empty();
 	});
 
 	t1.join();
 	t2.join();
+	t3.join();
 }
--- src/synchronized.hpp
+++ src/synchronized.hpp
@@ -1,33 +1,100 @@
 #include <mutex>
+#include <shared_mutex>
 #include <utility>
 #include <type_traits>
 
 namespace detail
 {
-	template<typename U>
+	template<typename T, typename Lock = std::mutex>
 	class locker
 	{
 	public:
-		locker(U& v, std::recursive_mutex& l) noexcept
-		: m_value(v), m_lock(l) { m_lock.lock(); }
+		using mutex_type = Lock;
+
+		locker(T& v, Lock& l)
+		: m_value(v), m_guard(l) {}
 
 		locker(const locker&) = delete;
 		locker& operator = (const locker&) = delete;
 
-		~locker() noexcept { m_lock.unlock(); }
-
-		U* operator -> () noexcept { return &m_value; }
+		T* operator -> () { return &m_value; }
+		const T* operator -> () const { return &m_value; }
 
 	private:
-		U& m_value;
-		std::recursive_mutex& m_lock;
+		T& m_value;
+		std::scoped_lock<Lock> m_guard;
+	};
+
+	template<typename T, typename Lock = std::mutex>
+	class const_locker
+	{
+	public:
+		using mutex_type = Lock;
+
+		const_locker(T& v, Lock& l)
+		: m_value(v), m_guard(l) {}
+
+		const_locker(const const_locker&) = delete;
+		const_locker& operator = (const const_locker&) = delete;
+
+		const T* operator -> () const { return &m_value; }
+
+	private:
+		T& m_value;
+		std::scoped_lock<Lock> m_guard;
+	};
+
+
+
+	template<typename T, typename Lock = std::shared_mutex>
+	class shared_locker
+	{
+	public:
+		using mutex_type = Lock;
+
+		shared_locker(T& v, Lock& l)
+		: m_value(v), m_guard(l) {}
+
+		shared_locker(const shared_locker&) = delete;
+		shared_locker& operator = (const shared_locker&) = delete;
+
+		T* operator -> () { return &m_value; }
+		const T* operator -> () const { return &m_value; }
+
+	private:
+		T& m_value;
+		std::unique_lock<Lock> m_guard;
+	};
+
+	template<typename T, typename Lock = std::shared_mutex>
+	class const_shared_locker
+	{
+	public:
+		using mutex_type = Lock;
+
+		const_shared_locker(T& v, Lock& l)
+		: m_value(v), m_guard(l) {}
+
+		const_shared_locker(const const_shared_locker&) = delete;
+		const_shared_locker& operator = (const const_shared_locker&) = delete;
+
+		const T* operator -> () const { return &m_value; }
+
+	private:
+		T& m_value;
+		std::shared_lock<Lock> m_guard;
 	};
 }
 
-template<typename T>
+
+
+template<typename T, typename Lock = std::mutex>
 class synchronized
 {
 public:
+	using value_type = T;
+	using mutex_type = Lock;
+
 	synchronized() = default;
 	synchronized(const T& v) : m_value(v) {}
 	synchronized(T&& v) : m_value(std::move(v)) {}
@@ -43,12 +110,56 @@ public:
 	synchronized(const synchronized&) = delete;
 	synchronized& operator = (const synchronized&) = delete;
 
-	detail::locker<T> operator -> () noexcept
+	detail::locker<T, Lock> operator -> ()
 	{
 		return detail::locker(m_value, m_lock);
 	}
 
+	detail::const_locker<const T, Lock> operator -> () const
+	{
+		return detail::const_locker(m_value, m_lock);
+	}
+
 private:
-	T m_value;
-	std::recursive_mutex m_lock;
+	T m_value = T();
+	mutable Lock m_lock;
+};
+
+
+
+template<typename T, typename Lock = std::shared_mutex>
+class shared_synchronized
+{
+public:
+	using value_type = T;
+	using mutex_type = Lock;
+
+	shared_synchronized() = default;
+	shared_synchronized(const T& v) : m_value(v) {}
+	shared_synchronized(T&& v) : m_value(std::move(v)) {}
+
+	template<typename... A, std::enable_if_t<
+		std::is_constructible_v<T, A...>>* = nullptr>
+	shared_synchronized(A&&... a) : m_value(std::forward<A>(a)...) {}
+
+	template<typename V, std::enable_if_t<
+		std::is_constructible_v<T, std::initializer_list<V>>>* = nullptr>
+	shared_synchronized(std::initializer_list<V> l) : m_value(l) {}
+
+	shared_synchronized(const shared_synchronized&) = delete;
+	shared_synchronized& operator = (const shared_synchronized&) = delete;
+
+	detail::shared_locker<T, Lock> operator -> ()
+	{
+		return detail::shared_locker(m_value, m_lock);
+	}
+
+	detail::const_shared_locker<const T, Lock> operator -> () const
+	{
+		return detail::const_shared_locker(m_value, m_lock);
+	}
+
+private:
+	T m_value = T();
+	mutable Lock m_lock;
 };
