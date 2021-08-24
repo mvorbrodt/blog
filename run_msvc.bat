77aaa5210156b02a2ae2810ac3948cab0ccac39e
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2021-08-23 22:02:33 -0400

    VS 2019 cmake script

--- run_msvc.bat
+++ run_msvc.bat
@@ -5,13 +5,16 @@ copy /Y src\CMakeLists.txt.msvc src\CMakeLists.txt
 
 del /F /S /Q msvc
 del /F /Q msvc
+
 mkdir msvc
 cd msvc
 
 cmake -DCMAKE_TOOLCHAIN_FILE=C:\Code\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows ..
-cd ..
+
+REM msbuild -m blog.sln -t:Clean -p:Configuration=Debug
+REM msbuild -m blog.sln -t:Rebuild -p:Configuration=Debug
 
 REM msbuild -m blog.sln -t:Clean -p:Configuration=Release
 REM msbuild -m blog.sln -t:Rebuild -p:Configuration=Release
-REM git checkout CMakeLists.txt
-REM git checkout src\CMakeLists.txt
+
+pause
