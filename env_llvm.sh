b51efbb7bdaf272ae6e21e0ed011b16811e298ab
mvorbrodt <martin.vorbrodt@icloud.com>
2021-09-02 16:48:09 -0400

    more llvm build scripts

---
+++ env_llvm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export CC="/usr/local/bin/clang"
+export CXX="/usr/local/bin/clang++"
+export LD="/usr/local/bin/ld.lld"
+export AR="/usr/local/bin/llvm-ar"
+export RANLIB="/usr/local/bin/llvm-ranlib"
+
+export CPPFLAGS="-I$(xcrun --show-sdk-path)/usr/include"
+# export CPPFLAGS="-I$(xcrun --show-sdk-path)/usr/include -I/usr/local/include/c++/v1 -I/usr/local/include"
+export CXXFLAGS="$CPPFLAGS"
+export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
+
+alias cc=$CC
+alias c++=$CXX
+alias ld=$LD
+alias ar=$AR
+alias ranlib=$RANLIB
--- run_llvm.sh
+++ run_llvm.sh
@@ -1,29 +1,14 @@
 #!/bin/sh
 
-export CC="/usr/local/bin/clang"
-export CXX="/usr/local/bin/clang++"
-export LD="/usr/local/bin/ld.lld"
-export AR="/usr/local/bin/llvm-ar"
-export RANLIB="/usr/local/bin/llvm-ranlib"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-export CPPFLAGS="-I/usr/local/include/c++/v1 -I/usr/local/include"
-export CXXFLAGS="$CPPFLAGS"
-
-alias cc=$CC
-alias c++=$CXX
-alias ld=$LD
-alias ar=$AR
-alias ranlib=$RANLIB
-
 cd ~/Code/blog
 ln -sf CMakeLists.txt.llvm CMakeLists.txt
-
 cd src
 ln -sf CMakeLists.txt.llvm CMakeLists.txt
 cd ..
+
+source env_llvm.sh
 
 rm -rf llvm 2> /dev/null
 mkdir llvm 2> /dev/null
 cd llvm
 cmake ..
-cd ..
