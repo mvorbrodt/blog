114bea70914b121b55420367013a261bb8c524a4
mvorbrodt <martin.vorbrodt@icloud.com>
2022-03-14 17:26:49 -0400

    find openssl@3

--- run_llvm.sh
+++ run_llvm.sh
@@ -7,6 +7,7 @@ ln -sf CMakeLists.txt.llvm CMakeLists.txt
 cd ..
 
 source env_llvm.sh
+export OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl@3/3.0.1
 
 rm -rf llvm 2> /dev/null
 mkdir llvm 2> /dev/null
--- run_xcode.sh
+++ run_xcode.sh
@@ -7,6 +7,8 @@ cd src
 ln -sf CMakeLists.txt.llvm CMakeLists.txt
 cd ..
 
+export OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl@3/3.0.1
+
 mkdir xcode 2> /dev/null
 cd xcode
 rm CMakeCache.txt 2> /dev/null
