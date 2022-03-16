3ba103b83b019ff24c2655afe2381d8a5182057a
mvorbrodt <martin.vorbrodt@icloud.com>
2022-03-15 20:49:15 -0400

    cmake updates

--- run_llvm.sh
+++ run_llvm.sh
@@ -7,9 +7,8 @@ ln -sf CMakeLists.txt.llvm CMakeLists.txt
 cd ..
 
 source env_llvm.sh
-export OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl@3/3.0.1
 
 rm -rf llvm 2> /dev/null
 mkdir llvm 2> /dev/null
 cd llvm
-cmake ..
+cmake -DCMAKE_OSX_SYSROOT="$(xcrun --show-sdk-path)" ..
--- run_xcode.sh
+++ run_xcode.sh
@@ -7,10 +7,8 @@ cd src
 ln -sf CMakeLists.txt.llvm CMakeLists.txt
 cd ..
 
-export OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl@3/3.0.1
-
 mkdir xcode 2> /dev/null
 cd xcode
 rm CMakeCache.txt 2> /dev/null
-cmake -G Xcode -T buildsystem=1 ..
+cmake -DCMAKE_OSX_SYSROOT="$(xcrun --show-sdk-path)" -G Xcode -T buildsystem=1 ..
 cd ..
