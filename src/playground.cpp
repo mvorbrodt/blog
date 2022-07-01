087a185211e0aa2ba77515715913ee2a66bf09a9
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2022-07-01 18:54:04 -0400

    structured binding example for...

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+#include <tuple>
 using namespace std;
+
+auto return_3() { return std::make_tuple("1"s, "2"s, "3"s); }
 
 int main()
 {
+	const auto& [s1, s2, s3] = return_3();
+	cout << s1 << endl << s2 << endl << s3 << endl;
 }
