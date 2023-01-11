99f23abca9857ebd55f65adc7f6cc3603f09fc8f
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2023-01-11 01:23:13 -0500

    Bloom filter improvements maybe?

--- src/bloom.cpp
+++ src/bloom.cpp
@@ -10,19 +10,19 @@ int main()
 	auto set1 = {"Martin", "Vorbrodt", "C++", "Blog"};
 	auto set2 = {"Not", "In", "The", "Set"};
 
-	bloom_filter<string, 5> bloom(128);
+	bloom_filter<string> bloom(128/*, 3*/);
 	for(auto s : set1) bloom.add(s);
 
 	std::boolalpha(cout);
-	cout << "bloom_filter<string, 5> bloom(128);" << endl;
+	cout << "bloom_filter:" << endl;
 	for(auto s : set1) cout << "\tContains \"" << s << "\"\t: " << bloom.contains(s) << endl;
 	for(auto s : set2) cout << "\tContains \"" << s << "\"\t: " << bloom.contains(s) << endl;
 	cout << endl;
 
-	fixed_bloom_filter<string, 128, 1> fixed_bloom;
+	fixed_bloom_filter<string, 128/*, 3*/> fixed_bloom;
 	for(auto s : set1) fixed_bloom.add(s);
 
-	cout << "fixed_bloom_filter<string, 128, 1> fixed_bloom;" << endl;
+	cout << "fixed_bloom_filter:" << endl;
 	for(auto s : set1) cout << "\tContains \"" << s << "\"\t: " << fixed_bloom.contains(s) << endl;
 	for(auto s : set2) cout << "\tContains \"" << s << "\"\t: " << fixed_bloom.contains(s) << endl;
 }
