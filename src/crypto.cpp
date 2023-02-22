4d7b6388cb082ca4e167f44a0e7d0bb7fd7f9ad9
Martin Vorbrodt <martin.vorbrodt@citrix.com>
2023-02-22 14:46:26 -0500

    playing with tokens and cryptos

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -5,9 +5,87 @@
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/files.h>
+
+#include <memory>
+#include <utility>
+#include <fstream>
+#include <ios>
+#include <stdexcept>
+#include <vector>
+#include <cstdint>
+#include <botan/auto_rng.h>
+#include <botan/rsa.h>
+#include <botan/x509_key.h>
+#include <botan/pkcs8.h>
+#include <botan/pk_keys.h>
+#include <botan/pubkey.h>
+
 #include <botan/hash.h>
 #include <botan/hex.h>
 #include <wolfssl/wolfcrypt/sha.h>
+
+class RSAKeyPair;
+using RSAKeyPairPtr = std::unique_ptr<RSAKeyPair>;
+
+class RSAKeyPair
+{
+public:
+	static auto Create(std::size_t bits = 4096)
+	{
+		return RSAKeyPair(std::make_unique<Botan::RSA_PrivateKey>(kRNG, bits));
+	}
+
+	static void Save(const RSAKeyPair& key, const std::string& name, const std::string& pass)
+	{
+		auto text = Botan::PKCS8::PEM_encode(*key.m_key.get(), kRNG, pass);
+
+		std::ofstream ofs;
+		ofs.exceptions(std::ios::failbit | std::ios::badbit);
+		ofs.open(name, std::ios::out | std::ios::trunc);
+		ofs.write(text.c_str(), text.length());
+		ofs.close();
+	}
+
+	static auto CreateAndSave(const std::string& name, const std::string& pass, std::size_t bits = 4096)
+	{
+		auto key = Create(bits);
+		Save(key, name, pass);
+		return std::move(key);
+	}
+
+	static auto Load(const std::string& file, const std::string& pass, bool check = true, bool strong = false)
+	{
+		auto key = std::unique_ptr<Botan::Private_Key>(Botan::PKCS8::load_key(file, kRNG, pass));
+		if (check && !key->check_key(kRNG, strong))
+			throw std::invalid_argument("Invalid key!");
+
+		return RSAKeyPair(std::move(key));
+	}
+
+	auto Encrypt(const std::string& str) const
+	{
+		return m_enc->encrypt((const std::uint8_t*)str.data(), str.length(), kRNG);
+	}
+
+	auto Encrypt(const std::vector<std::byte>& data) const
+	{
+		return m_enc->encrypt((const std::uint8_t*)data.data(), data.size(), kRNG);
+	}
+
+private:
+	RSAKeyPair(std::unique_ptr<Botan::Private_Key>&& key)
+	: m_key(std::move(key)),
+	m_enc(std::make_unique<Botan::PK_Encryptor_EME>(*m_key, kRNG, "EME1(SHA-256)")),
+	m_dec(std::make_unique<Botan::PK_Decryptor_EME>(*m_key, kRNG, "EME1(SHA-256)")) {}
+
+	std::unique_ptr<Botan::Private_Key> m_key;
+	std::unique_ptr<Botan::PK_Encryptor_EME> m_enc;
+	std::unique_ptr<Botan::PK_Decryptor_EME> m_dec;
+
+	inline static Botan::AutoSeeded_RNG kRNG;
+};
+
+
 
 using namespace std;
 
@@ -52,6 +130,10 @@ string Hash_Botan(const string& msg)
 
 int main()
 {
+	// RSAKeyPair::CreateAndSave("key.txt", "123", 1024);
+	// auto key = RSAKeyPair::Load("key.txt", "123");
+	// return 0;
+
 	std::string msg = "Vorbrodt's C++ Blog @ https://vorbrodt.blog";
 
 	cout << "Message: " << msg << endl;
--- src/playground.cpp
+++ src/playground.cpp
@@ -1,12 +1,149 @@
-#include <iostream>
-#include <string>
-#include <tuple>
-using namespace std;
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <stdexcept>
 
-auto return_3() { return std::make_tuple("1"s, "2"s, "3"s); }
+template<typename Resolution = std::chrono::nanoseconds>
+class token_bucket
+{
+public:
+	token_bucket(std::uint64_t tokens_per_second, std::uint64_t token_capacity)
+	: token_bucket(std::chrono::duration_cast<Resolution>(std::chrono::seconds(1)) / tokens_per_second, token_capacity) {}
+
+	template<typename Rep, typename Period>
+	token_bucket(const std::chrono::duration<Rep, Period>& time_per_token, std::uint64_t token_capacity)
+	: m_time_per_token{ static_cast<std::uint64_t>(std::chrono::duration_cast<Resolution>(time_per_token).count()) },
+		m_token_capacity{ m_time_per_token * token_capacity }
+	{
+		if(std::chrono::duration_cast<std::chrono::nanoseconds>(time_per_token).count()
+			< std::chrono::duration_cast<std::chrono::nanoseconds>(Resolution(1)).count())
+			throw std::invalid_argument("Invalid resolution!");
+		if(!time_per_token.count() || !token_capacity)
+			throw std::invalid_argument("Invalid rate or capacity!");
+	}
+
+	token_bucket(const token_bucket& other)
+	: m_time{ other.m_time.load(std::memory_order_relaxed) }, m_time_per_token{ other.m_time_per_token }, m_token_capacity{ other.m_token_capacity } {}
+
+	token_bucket(token_bucket&& other)
+	: m_time{ other.m_time.load(std::memory_order_relaxed) }, m_time_per_token{ other.m_time_per_token }, m_token_capacity{ other.m_token_capacity } {}
+
+	token_bucket& operator = (token_bucket other)
+	{
+		m_time = other.m_time.load(std::memory_order_relaxed);
+		m_time_per_token = other.m_time_per_token;
+		m_token_capacity = other.m_token_capacity;
+		return *this;
+	}
+
+	[[nodiscard]] bool try_consume(std::uint64_t tokens = 1, Resolution* time_needed = nullptr)
+	{
+		auto now = static_cast<std::uint64_t>(std::chrono::duration_cast<Resolution>(std::chrono::steady_clock::now().time_since_epoch()).count());
+		auto delay = tokens * m_time_per_token;
+		auto min_time = now - m_token_capacity;
+		auto old_time = m_time.load(std::memory_order_relaxed);
+		auto new_time = old_time;
+
+		if (min_time > old_time)
+			new_time = min_time;
+
+		while (true)
+		{
+			new_time += delay;
+
+			if (new_time > now)
+			{
+				if (time_needed != nullptr)
+					*time_needed = Resolution(new_time - now);
+	
+				return false;
+			}
+
+			if (m_time.compare_exchange_weak(old_time, new_time, std::memory_order_relaxed, std::memory_order_relaxed))
+				return true;
+
+			new_time = old_time;
+		}
+
+		[[unlikely]] return false;
+	}
+
+	void consume(std::uint64_t tokens = 1)
+	{
+		while (!try_consume(tokens))
+			std::this_thread::yield();
+	}
+
+	void wait(std::uint64_t tokens = 1)
+	{
+		Resolution time_needed{};
+		while (!try_consume(tokens, &time_needed))
+			std::this_thread::sleep_for(time_needed);
+	}
+
+private:
+	std::atomic_uint64_t m_time = 0;
+	std::uint64_t m_time_per_token;
+	std::uint64_t m_token_capacity;
+};
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <syncstream>
+#include <vector>
+#include <latch>
+#include <barrier>
+
+#define ss std::osyncstream(std::cout)
 
 int main()
 {
-	const auto& [s1, s2, s3] = return_3();
-	cout << s1 << endl << s2 << endl << s3 << endl;
+	using namespace std;
+	using namespace std::chrono;
+
+	auto count = 3;// thread::hardware_concurrency();
+	auto run = atomic_bool{ true };
+	auto total = atomic_uint64_t{};
+	auto counts = vector<atomic_uint64_t>(count);
+	auto threads = vector<jthread>(count);
+	auto bucket = token_bucket(3us, 100);
+
+	auto b2 = bucket;
+	auto b3 = move(b2);
+	bucket = b3;
+	bucket = move(b2);
+	
+	auto fair_start = latch(count);
+	auto fair_play = barrier(count);
+
+	auto worker = [&](auto x)
+	{
+		fair_start.arrive_and_wait();
+		while (run)
+		{
+			//fair_play.arrive_and_wait();
+			bucket.consume();
+			++total, ++counts[x];
+		}
+		fair_play.arrive_and_drop();
+	};
+
+	for (auto& t : threads)
+		t = jthread(worker, --count);
+
+	thread([&]
+	{
+		while (run)
+		{
+			this_thread::sleep_for(1.5s);
+			for (auto& count : counts)
+				ss << fixed << setprecision(5) << left << count << "\t" << (100.0 * count / total) << "%" << endl;
+			ss << endl;
+		}
+	}).detach();
+
+	cin.get();
+	run = false;
 }
