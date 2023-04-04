50d9fb940c015005d18dc9d3bbaa981d24205894
Martin Vorbrodt <martin.vorbrodt@citrix.com>
2023-04-04 18:22:33 -0400

    playing with boxing/type erasure

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,96 +1,121 @@
 #include <iostream>
-#include <string>
-#include <atomic>
-#include <thread>
-#include <vector>
-#include <array>
-#include <latch>
-#include <random>
-#include <new>
-#include <condition_variable>
+#include <memory>
+#include <utility>
+#include <typeinfo>
+#include <type_traits>
 
-#ifdef __cpp_lib_hardware_interference_size
-	using std::hardware_constructive_interference_size;
-	using std::hardware_destructive_interference_size;
-#else
-	constexpr std::size_t hardware_constructive_interference_size = 64;
-	constexpr std::size_t hardware_destructive_interference_size = 64;
-#endif
 
-template<typename T = int, std::size_t N = hardware_constructive_interference_size / sizeof(T)>
-struct Numbers
+
+class i_boxed
 {
-	static constexpr auto Size  = sizeof(T);
-	static constexpr auto Count = N;
-	static constexpr auto Bytes = Size * Count;
+public:
+	virtual ~i_boxed() = default;
 
-	void inc()
-	{
-		while (lock.test_and_set(std::memory_order_acquire));
-		for (auto& n : nums)
-			++n;
-		lock.clear(std::memory_order_release);
-	}
+	virtual std::size_t hash() const = 0;
 
-	auto sum()
-	{
-		T s = {};
-		for (auto n : nums)
-			s += n;
-		return s;
-	}
-
-	std::array<T, N> nums = {};
-	std::atomic_flag lock = ATOMIC_FLAG_INIT;
+	virtual std::unique_ptr<i_boxed> clone() const = 0;
 };
 
-using Nums = Numbers<>;
 
-#define all(x) for(auto& i : x) i
 
-struct null_mutex
+template<typename T>
+class boxed : public i_boxed
 {
-	void lock() {}
-	void unlock() {}
+public:
+	template<typename U = T>
+	explicit boxed(U&& v) : m_h{ typeid(T).hash_code() }, m_v { std::forward<U>(v) } {}
+
+	virtual std::size_t hash() const final override { return m_h; }
+
+	virtual std::unique_ptr<i_boxed> clone() const final override { return std::make_unique<boxed<T>>(*this); }
+
+	T& value() { return m_v; }
+
+	const T& value() const { return m_v; }
+
+private:
+	std::size_t m_h;
+	T m_v;
 };
-auto nm = null_mutex{};
-auto cv = std::condition_variable_any{};
+
+
+
+class box
+{
+public:
+	template<typename T>
+	explicit box(T&& v) : m_v{ std::make_unique<boxed<T>>(std::forward<T>(v)) } {}
+
+	box(const box& o) : m_v{ o.m_v->clone() } {}
+
+	box(box&&) = default;
+
+	box& operator = (const box& o)
+	{
+		if (this == &o)
+			return *this;
+
+		m_v = o.m_v->clone();
+		return *this;
+	}
+
+	box& operator = (box&&) = default;
+
+	operator bool() const { return !!m_v; }
+
+	bool operator ! () const { return !m_v; }
+
+	template<typename T>
+	[[nodiscard]] friend T& unbox(box& a)
+	{
+		if (typeid(T).hash_code() != a.m_v->hash())
+			throw std::bad_cast();
+
+		return static_cast<boxed<T>*>(a.m_v.get())->value();
+	}
+
+	template<typename T>
+	[[nodiscard]] friend const T& unbox(const box& a)
+	{
+		if (typeid(T).hash_code() != a.m_v->hash())
+			throw std::bad_cast();
+
+		return static_cast<const boxed<T>*>(a.m_v.get())->value();
+	}
+
+private:
+	std::unique_ptr<i_boxed> m_v;
+};
+
+
 
 int main()
 {
 	using namespace std;
 
-	auto str = "Hello C++"s;
-	auto proc = [] (string&& str) { cout << str << endl; };
-	cout << str << endl;
-	proc(std::move(str));
-	cout << str << endl;
-	return 1;
-
-	thread([&] { cout << "waiting..." << endl; cv.wait(nm); cout << "...done!" << endl; }).detach();
-	this_thread::sleep_for(5s);
-	cv.notify_one();
-	this_thread::sleep_for(1s);
-	return 1;
-
-	auto N = default_random_engine{ random_device{}() }() % 1'000'000;
-	auto Ts = thread::hardware_concurrency();
-	auto fair_start = latch{ Ts };
-	auto threads = vector<thread>{ Ts };
-	auto nums = Nums{};
-	auto expected = Nums::Count * N * Ts;
-
-	cout << Nums::Count << " numbers, " << Nums::Size << " bytes each, " << Nums::Bytes << " total, " << Ts << " threads and " << N << " iterations" << endl;
-
-	all(threads) = thread([&]
+	try
 	{
-		fair_start.arrive_and_wait();
-		for (auto i = 0; i < N; ++i)
-			nums.inc();
-	});
+		box x(1);
+		box y(2);
+		const box z(3);
 
-	all(threads).join();
+		cout << unbox<int>(x) << endl;
+		cout << unbox<int>(y) << endl;
+		cout << unbox<int>(z) << endl;
 
-	cout << "expected = " << expected << endl;
-	cout << "actual   = " << nums.sum() << endl;
+		box a = x;
+		box b = std::move(y);
+
+		y = b;
+		x = std::move(y);
+
+		if (x) cout << "x has value!" << endl;
+		if (!y) cout << "y has NO value!" << endl;
+
+		[[maybe_unused]] auto q = unbox<float>(x);
+	}
+	catch (exception& e)
+	{
+		cerr << e.what() << endl;
+	}
 }
